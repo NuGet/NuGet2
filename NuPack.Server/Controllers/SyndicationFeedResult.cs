0b40ddb8fae6b0fc852063a77b728471bbac77ea
dfowler <davidfowl@gmail.com>
2010-09-01 00:21:55 -0700

    - Added mvc project that hosts a package feed with one demo package.

---
+++ NuPack.Server/Controllers/SyndicationFeedResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ServiceModel.Syndication;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace NuPack.Server.Controllers {
+    public class SyndicationFeedResult : ActionResult {
+        private string _contentType;
+        private SyndicationFeed _feed;
+        private Func<SyndicationFeed, SyndicationFeedFormatter> _formatterFactory;
+
+        public SyndicationFeedResult(SyndicationFeed feed,
+                                     Func<SyndicationFeed, SyndicationFeedFormatter> formatterFactory,
+                                     string contentType) {
+            _feed = feed;
+            _formatterFactory = formatterFactory;
+            _contentType = contentType;
+        }
+
+        public override void ExecuteResult(ControllerContext context) {
+            context.RequestContext.HttpContext.Response.ContentType = _contentType;
+
+            using (var writer = XmlWriter.Create(context.RequestContext.HttpContext.Response.Output)) {
+                var formatter = _formatterFactory(_feed);
+                formatter.WriteTo(writer);
+            }
+        }
+    }
+}
---
+++ NuPack.Server/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NuPack.Server.MvcApplication" Language="C#" %>
