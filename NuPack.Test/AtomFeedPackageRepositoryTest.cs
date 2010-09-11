98c79b5fd91683665f45c41bb73c8c4453ca8dc7
pranavkm <prkrishn@hotmail.com>
2010-09-11 13:47:15 -0700

    Adding unit tests to check for AtomFeedRepository exception handling

---
+++ NuPack.Test/AtomFeedPackageRepositoryTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NuPack.Test {
+    [TestClass]
+    public class AtomFeedPackageRepositoryTest {
+        [TestMethod]
+        public void AtomFeedConstructedithNullUriThrowsException() {
+            // Act and Assert
+            ExceptionAssert.ThrowsArgNull(() => new AtomFeedPackageRepository(null), "feedUri");
+        }
+
+        [TestMethod]
+        public void AtomFeedThrowsWhenLoadingFromInvalidFeedSource() {
+            // Arrange
+            var feedUri = new Uri("http://some-website-that-hopefully-should-not-exist/foo-bar");
+            var repo = new AtomFeedPackageRepository(feedUri);
+
+            // Act and Assert
+            ExceptionAssert.Throws<InvalidOperationException>(() => repo.GetFeedItems(), "Unable to read feed. Verify that a feed is hosted at the remote server and is available.");
+        }
+
+        [TestMethod]
+        public void AtomFeedThrowsWhenReadingInvalidContent() {
+            // Arrange
+            var xmlReader = new XmlTextReader(new StringReader("malformed xml"));
+
+            // Act and Assert
+            ExceptionAssert.Throws<InvalidOperationException>(
+                () => AtomFeedPackageRepository.GetFeedItems(xmlReader),
+                "Unable to read feed contents. Verify that the feed conforms to the Atom Syndication format.");
+        }
+    }
+}
