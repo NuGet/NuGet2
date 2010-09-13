e1455fe4193215de8b13ef857f8f54aa724ee25c
dfowler <davidfowl@gmail.com>
2010-09-12 20:01:17 -0700

    - Removed solution folder - Added ISourceControlFileSystemProvider interface to NuPack.VisualStudio - Added TfsFileSystem and TfsSourceControlFileSystemProvider in new assembly - Load the correct implementation of ISourceControlFileSystemProvider for the current solution based on source control bindings - Added NuPack.TeamFoundationServer as MEF component in VSIX

---
+++ NuPack.TeamFoundationServer/TfsWorkspaceWrapper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.TeamFoundation.VersionControl.Client;
+
+namespace NuPack.TeamFoundationServer {
+    public class TfsWorkspaceWrapper : ITfsWorkspace {
+        private readonly Workspace _workspace;
+        public TfsWorkspaceWrapper(Workspace workspace) {
+            _workspace = workspace;
+        }
+
+        public bool PendEdit(string path) {
+            return _workspace.PendEdit(path) > 0;
+        }
+
+        public bool PendAdd(string path) {
+            return _workspace.PendAdd(path) > 0;
+        }
+
+        public IEnumerable<string> GetItems(string path) {
+            return GetItems(path, ItemType.Any);
+        }
+
+        public IEnumerable<string> GetItems(string path, ItemType itemType) {
+            // REVIEW: We should pass the filter to this method so it happens on the server
+            // REVIEW: Can we do some smart caching so we don't hit the server everytime this is called?
+            var itemSet = _workspace.VersionControlServer.GetItems(path, VersionSpec.Latest, RecursionType.OneLevel, DeletedState.NonDeleted, itemType);
+            
+            // Get the local files for the server files
+            var items = new HashSet<string>(from item in itemSet.Items
+                                            select GetLocalItemForServerItem(item.ServerItem),
+                                            StringComparer.OrdinalIgnoreCase);
+
+            // Remove the path from the list if we're looking for folders
+            if (itemType == ItemType.Folder) {
+                items.Remove(path);
+
+                if (Directory.Exists(path)) {
+                    // Files might have pending adds, but directories might not
+                    // (even though they get added as a result of checking in those files).
+                    foreach (var directory in Directory.EnumerateDirectories(path)) {
+                        items.Add(directory);
+                    }
+                }
+            }
+
+            // Remove pending deletes from the items collection
+            foreach (var change in GetPendingChanges(path, RecursionType.OneLevel)) {
+                if (change.IsDelete) {
+                    // Remove all children of this path
+                    items.RemoveWhere(item => item.StartsWith(change.LocalItem, StringComparison.OrdinalIgnoreCase));
+                }
+                else {
+                    items.Add(change.LocalItem);
+                }
+            }
+
+            return items;
+        }
+
+        public IEnumerable<PendingChange> GetPendingChanges(string path) {
+            return _workspace.GetPendingChangesEnumerable(path);
+        }
+
+        public IEnumerable<PendingChange> GetPendingChanges(string path, RecursionType recursionType) {
+            return _workspace.GetPendingChangesEnumerable(path, recursionType);
+        }
+
+        public string GetLocalItemForServerItem(string path) {
+            return _workspace.GetLocalItemForServerItem(path);
+        }
+
+        public void Undo(IEnumerable<PendingChange> pendingChanges) {
+            if (pendingChanges.Any()) {
+                _workspace.Undo(pendingChanges.ToArray());
+            }
+        }
+
+        public bool PendDelete(string path, RecursionType recursionType) {
+            return _workspace.PendDelete(path, recursionType) > 0;
+        }
+
+
+        public bool ItemExists(string path) {
+            // TODO: Find a better way to implement this
+            return GetItems(path).Any();
+        }
+    }
+}
