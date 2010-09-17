51e59be0de9236886edbec4c2b5849c8000a3557
davidebbo <david.ebbo@microsoft.com>
2010-09-17 01:03:46 -0700

    New package to support Ninject in MVC3

---
+++ PackageSources/Ninject.MVC3/Content/NinjectMVC3.cs.pp
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Ninject;
+using Ninject.Mvc3;
+
+namespace $rootnamespace$ {
+    public class NinjectMVC3 {
+        public static void RegisterServices(IKernel kernel) {
+            //kernel.Bind<IThingRepository>().To<SqlThingRepository>();
+        }
+
+        public static void SetupDependencyInjection() {
+            // Create Ninject DI Kernel 
+            IKernel kernel = new StandardKernel();
+
+            // Register services with our Ninject DI Container
+            RegisterServices(kernel);
+
+            // Tell ASP.NET MVC 3 to use our Ninject DI Container 
+            MvcServiceLocator.SetCurrent(new NinjectServiceLocator(kernel));
+        }
+    }
+}
