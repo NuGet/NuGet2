412639c8a693f729b8098c5a1a8a004e02cf863b
dotnetjunky <kinhln@microsoft.com>
2010-09-27 16:40:54 -0700

    Move the disclaimer text inside the ExtensionsExplorer control, rather than at the top of the dialog, as per spec.
    --HG--
    extra : rebase_source : b8c98854f6eb148a547527f72082d473ca41d38f

--- NuPack.Dialog/PackageManagerUI/LicenseAcceptanceWindow.xaml.cs
+++ NuPack.Dialog/PackageManagerUI/LicenseAcceptanceWindow.xaml.cs
@@ -31,8 +31,8 @@ namespace NuPack.Dialog.PackageManagerUI {
             }
 
             string scheme = licenseUrl.Scheme;
-            if (scheme.Equals("http", StringComparison.OrdinalIgnoreCase) ||
-                scheme.Equals("https", StringComparison.OrdinalIgnoreCase)) {
+            if (scheme.Equals(Uri.UriSchemeHttp, StringComparison.OrdinalIgnoreCase) ||
+                scheme.Equals(Uri.UriSchemeHttps, StringComparison.OrdinalIgnoreCase)) {
                 // REVIEW: Will this allow a package author to execute arbitrary program on user's machine?
                 // We have limited the url to be HTTP only, but is it sufficient?
                 System.Diagnostics.Process.Start(licenseUrl.AbsoluteUri);
