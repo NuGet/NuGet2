1ddcb2bd002866396a38ef19616a9266c58cbc8b
dotnetjunky <kinhln@microsoft.com>
2010-10-05 11:02:48 -0700

    Add LoggerDisposer class, which was missing in the previous commit.
    --HG--
    extra : rebase_source : df0705e6056b8d2212362abf8518c6e7b572d50c

---
+++ NuPack.VisualStudio/Cmdlets/LoggerDisposer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NuPack.VisualStudio.Cmdlets {
+    internal class LoggerDisposer : IDisposable {
+        private dynamic _loggerHost;
+
+        public LoggerDisposer(dynamic loggerHost, ILogger logger) {
+            _loggerHost = loggerHost;
+            loggerHost.Logger = logger;
+        }
+
+        public void Dispose() {
+            _loggerHost.Logger = null;
+        }
+    }
+}
