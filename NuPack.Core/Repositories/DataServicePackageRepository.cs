5678e6859b8d35978a882965078844cb3bbc8025
dfowler <davidfowl@gmail.com>
2010-10-04 22:32:27 -0700

    Add initial DataService repository.
    --HG--
    extra : rebase_source : d226cce9e6d18d9dfe29c78e899159890ee4ab2a

---
+++ NuPack.Core/Repositories/DataServicePackageRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace NuPack.Repositories {
+    using System;
+    using System.Data.Services.Client;
+    using System.Linq;
+    using System.Net.Cache;
+
+    public class DataServicePackageRepository : PackageRepositoryBase {
+        private readonly DataServiceContext _context;
+        private readonly string _packagesEntitySet;
+
+        public DataServicePackageRepository(Uri serviceRoot, string packagesEntitySet) {
+            _context = new DataServiceContext(serviceRoot);
+            _packagesEntitySet = packagesEntitySet;
+
+            _context.SendingRequest += OnSendingRequest;
+        }
+
+        private void OnSendingRequest(object sender, SendingRequestEventArgs e) {
+            // Use default credentials, configure the proxy
+            e.Request.UseDefaultCredentials = true;
+            Utility.ConfigureProxy(e.Request.Proxy);
+            // Use the default http cache policy
+            e.Request.CachePolicy = new HttpRequestCachePolicy();
+        }
+
+        public override IQueryable<IPackage> GetPackages() {
+            return _context.CreateQuery<IPackage>(_packagesEntitySet);
+        }
+    }
+}
