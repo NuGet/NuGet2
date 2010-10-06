e47834ff9b55da1d13c61c74d75be7a1f9bba82a
haacked <none@none>
2010-10-05 20:35:57 -0700

    Added a CREDITS.txt to reflect the inclusion of the ProvideBindingPath.cs file from the Visual Studio SDK. This file is dual licensed under the Visual Studio SDK license and the Apache 2.0 license. NuPack is taking it in under the Apache license and the header of that file has been updated to reflect that.

--- NuPack.Tools/ProvideBindingPath.cs
+++ NuPack.Tools/ProvideBindingPath.cs
@@ -1,20 +1,23 @@
-﻿/***************************************************************************
+﻿/*
+   Copyright 2010 Microsoft
 
-Copyright (c) Microsoft Corporation. All rights reserved.
-This code is licensed under the Visual Studio SDK license terms.
-THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-***************************************************************************/
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 using System;
 using System.Text;
-using System.IO;
 
-namespace Microsoft.VisualStudio.Shell
-{
+namespace Microsoft.VisualStudio.Shell {
     /// <summary>
     /// This attribute registers a path that should be probed for candidate assemblies at assembly load time.
     /// 
@@ -26,8 +29,7 @@ namespace Microsoft.VisualStudio.Shell
     /// for assemblies to load.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
-    public sealed class ProvideBindingPathAttribute : RegistrationAttribute
-    {
+    public sealed class ProvideBindingPathAttribute : RegistrationAttribute {
         /// <summary>
         /// An optional SubPath to set after $PackageFolder$. This should be used
         /// if the assemblies to be probed reside in a different directory than
@@ -35,23 +37,18 @@ namespace Microsoft.VisualStudio.Shell
         /// </summary>
         public string SubPath { get; set; }
 
-        private static string GetPathToKey(RegistrationContext context)
-        {
+        private static string GetPathToKey(RegistrationContext context) {
             return string.Concat(@"BindingPaths\", context.ComponentType.GUID.ToString("B").ToUpperInvariant());
         }
 
-        public override void Register(RegistrationContext context)
-        {
-            if (context == null)
-            {
+        public override void Register(RegistrationContext context) {
+            if (context == null) {
                 throw new ArgumentNullException("context");
             }
 
-            using (Key childKey = context.CreateKey(GetPathToKey(context)))
-            {
+            using (Key childKey = context.CreateKey(GetPathToKey(context))) {
                 StringBuilder keyName = new StringBuilder(context.ComponentPath);
-                if (!string.IsNullOrEmpty(SubPath))
-                {
+                if (!string.IsNullOrEmpty(SubPath)) {
                     keyName.Append("\\");
                     keyName.Append(SubPath);
                 }
@@ -60,10 +57,8 @@ namespace Microsoft.VisualStudio.Shell
             }
         }
 
-        public override void Unregister(RegistrationContext context)
-        {
-            if (context == null)
-            {
+        public override void Unregister(RegistrationContext context) {
+            if (context == null) {
                 throw new ArgumentNullException("context");
             }
 
