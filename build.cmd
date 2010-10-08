1b11599d65aef7adf85aa59c96c638b8b829fc5c
dotnetjunky <kinhln@microsoft.com>
2010-10-08 11:19:29 -0700

    Run Code Analysis for command line build. Don't run Code Analysis within IDE. Allow specifying configuration for build.exe.

--- build.cmd
+++ build.cmd
@@ -1,1 +1,5 @@
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj
+set config=%1
+if "%config%" == "" (
+   set config=debug
+)
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%"
