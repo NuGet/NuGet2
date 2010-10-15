31f83a4591f9eb7f6a9ec54fc49bc7c8d1f3b817
dotnetjunky <kinhln@microsoft.com>
2010-10-15 12:27:49 -0700

    Show the license link even if RequireLicenseAcceptance is false, as long as LicenseUrl is specified.

---
+++ NuPack.Dialog/PackageManagerUI/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NuPack.Dialog.PackageManagerUI {
+    public class NullToVisibilityConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            if (targetType == typeof(Visibility)) {
+                return value == null ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
