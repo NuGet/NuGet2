05258e159827b3c74584cdeaff2cd7c0a72871d6
dfowler <davidfowl@gmail.com>
2010-10-17 11:30:17 -0700

    - Fixed issue with null controller type and ninject controller factory in NuPack.Server. - Added unit test

--- NuPack.Server/Infrastructure/NinjectControllerFactory.cs
+++ NuPack.Server/Infrastructure/NinjectControllerFactory.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Web.Mvc;
-using Ninject;
 using System.Web.Routing;
+using Ninject;
 
 namespace NuPack.Server.Infrastructure {
     public class NinjectControllerFactory : DefaultControllerFactory {
         IKernel _kernel;
-        
+
         public NinjectControllerFactory(IKernel kernel) {
             _kernel = kernel;
         }
 
         protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType) {
-            return _kernel.TryGet(controllerType) as IController ?? base.GetControllerInstance(requestContext, controllerType);
+            if (controllerType != null) {
+                return (IController)_kernel.TryGet(controllerType) ?? base.GetControllerInstance(requestContext, controllerType);
+            }
+            return base.GetControllerInstance(requestContext, controllerType);
         }
 
         public virtual IController CreateControllerInstance(RequestContext requestContext, Type controllerType) {
--- NuPack.Test/Server/Infrastructure/NinjectControllerFactoryTest.cs
+++ NuPack.Test/Server/Infrastructure/NinjectControllerFactoryTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Ninject;
@@ -14,7 +16,7 @@ namespace NuPack.Test.Server.Infrastructure {
             // Arrange
             var controller = new TestController();
             var kernel = new Mock<IKernel>();
-            kernel.Setup(k => k.Resolve(It.IsAny<IRequest>())).Returns(new[]{controller});
+            kernel.Setup(k => k.Resolve(It.IsAny<IRequest>())).Returns(new[] { controller });
             var factory = new NinjectControllerFactory(kernel.Object);
 
             // Act
@@ -38,6 +40,22 @@ namespace NuPack.Test.Server.Infrastructure {
             Assert.IsInstanceOfType(controller, typeof(TestController));
         }
 
+        [TestMethod]
+        public void GetControllerInstanceFallsBackToDefaultBehaviorWhenControllerTypeIsNull() {
+            // Arrange
+            var kernel = new Mock<IKernel>();
+            kernel.Setup(k => k.Resolve(It.IsAny<IRequest>())).Returns(new Controller[] { });
+            var factory = new NinjectControllerFactory(kernel.Object);
+            var httpRequest = new Mock<HttpRequestBase>();
+            httpRequest.Setup(m => m.Path).Returns("FakePath");
+            var httpContext = new Mock<HttpContextBase>();
+            httpContext.Setup(m => m.Request).Returns(httpRequest.Object);
+            var requestContext = new Mock<RequestContext>();
+            requestContext.Setup(m => m.HttpContext).Returns(httpContext.Object);
+
+            // Act
+            ExceptionAssert.Throws<HttpException>(() => factory.CreateControllerInstance(requestContext.Object, null));
+        }
 
         public class TestController : IController {
             public void Execute(System.Web.Routing.RequestContext requestContext) {
