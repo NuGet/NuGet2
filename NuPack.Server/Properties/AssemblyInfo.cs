2b948660c4c8d12d487b3da07520a232f3409a47
pranavkm <prkrishn@hotmail.com>
2010-10-29 01:33:35 -0700

    Remove SecurityTransparent from Server

--- NuPack.Server/Properties/AssemblyInfo.cs
+++ NuPack.Server/Properties/AssemblyInfo.cs
@@ -1,7 +1,4 @@
 using System.Reflection;
-using System.Security;
 
 [assembly: AssemblyTitle("NuGet.Server")]
 [assembly: AssemblyDescription("Sample Web Application used to host a read-only NuGet feed")]
-
-[assembly: SecurityTransparent]
