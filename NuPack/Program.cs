c8f0062bf367f2912e9b7987539e9c9e6a30f57b
osbornm <matthew@osbornm.com>
2010-11-03 11:11:09 -0700

    Renaming the file with the main method in it to program.cs becuase every console app uses this convention
    --HG--
    extra : rebase_source : 64e1c835c21c6cc898b51baceedc3e78a3de6f1b

--- NuPack/Program.cs
+++ NuPack/Program.cs
@@ -7,7 +7,7 @@ namespace NuGet {
     using System.Linq;
     using System.Reflection;
 
-    public class PackageAuthoring {
+    public class Program {
         private HelpCommand _helpCommand;
         public HelpCommand HelpCommand {
             get {
@@ -35,7 +35,7 @@ namespace NuGet {
 
         public static int Main(string[] args) {
             // Import Dependecies  
-            var p = new PackageAuthoring();
+            var p = new Program();
             p.Initialize();
 
             // Add commands to the manager
