07c235107d1e7fc4bad60f36caf5afb36ee846ca
pranavkm <prkrishn@hotmail.com>
2010-11-05 12:30:32 -0700

    Perform BatchedQuerying only when Url is too long.
    --HG--
    extra : rebase_source : d92a1b20aa2ede098ba57a0a3e66d6b7636a9528

--- NuPack.Core/Repositories/BatchedDataServiceQuery.cs
+++ NuPack.Core/Repositories/BatchedDataServiceQuery.cs
@@ -8,8 +8,17 @@ using System.Linq.Expressions;
 using System.Reflection;
 
 namespace NuGet {
+    /// <summary>
+    /// Wrapper around OData's DataServiceRequest that switches to BatchQuery when the request URI becomes too long.
+    /// </summary>
+    /// <remarks>
+    /// Batch queries are multiple queries tunneled via a single post. Post requests are never cached, therefore it is imperative that batch queries are used sparingly. 
+    /// A formalized max-length is not specified, so we use 4k as per the analysis in http://www.boutell.com/newfaq/misc/urllength.html and only switch to batch queries when the url 
+    /// exceeds this limit.
+    /// </remarks>
     [SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", Justification="Type is an IQueryable and by convention should end with the term Query")]
     public class BatchedDataServiceQuery<T> : IQueryable<T>, IQueryProvider, IOrderedQueryable<T> {
+        private const int MaxUrlLength = 4000;
         private readonly DataServiceContext _context;
         private readonly IQueryable<T> _query;
 
@@ -23,12 +32,14 @@ namespace NuGet {
         }
 
         public IEnumerator<T> GetEnumerator() {
-            // REVIEW: Should we batch only if we're over the url limit?
-            var request = (DataServiceRequest)_query;
-            return _context.ExecuteBatch(request)
-                           .Cast<QueryOperationResponse>()
-                           .SelectMany(o => o.Cast<T>())
-                           .GetEnumerator();
+            var request = (DataServiceQuery)_query;
+            if (request.RequestUri.OriginalString.Length >= MaxUrlLength) {
+                return _context.ExecuteBatch(request)
+                               .Cast<QueryOperationResponse>()
+                               .SelectMany(o => o.Cast<T>())
+                               .GetEnumerator();
+            }
+            return request.Execute().Cast<T>().GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator() {
