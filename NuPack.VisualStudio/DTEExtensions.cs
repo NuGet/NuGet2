b8960ea3a971226b0229d9634f1bae1e796c6a3a
dfowler <davidfowl@gmail.com>
2010-11-06 14:31:31 -0700

    Cleanup DTEExtensions.
    --HG--
    extra : rebase_source : d3606e1d336af733fa209ce697d3981807fe982d

--- NuPack.VisualStudio/DTEExtensions.cs
+++ NuPack.VisualStudio/DTEExtensions.cs
@@ -1,25 +1,23 @@
 using System;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices;
 using EnvDTE;
-using Microsoft.VisualStudio.Shell;
 
 namespace NuGet.VisualStudio {
     public static class DTEExtensions {
         public static Project GetActiveProject(this _DTE dte) {
+            if (dte == null) {
+                throw new ArgumentException("dte");
+            }
+
             Project activeProject = null;
 
-            if (dte != null) {
-                Array activeProjects = dte.ActiveSolutionProjects as Array;
-                if (activeProjects != null && activeProjects.Length > 0) {
-                    Object projectValue = activeProjects.GetValue(0);
-                    Project project = projectValue as Project;
-                    if (project != null) {
-                        return project;
-                    }
+            var activeProjects = (object[])dte.ActiveSolutionProjects;
+            if (activeProjects != null && activeProjects.Length > 0) {
+                Project project = activeProjects[0] as Project;
+                if (project != null) {
+                    return project;
                 }
             }
+
             return activeProject;
         }
     }
