60d973e565c7cf3237315d12cb7529c2c6fe05a4
dfowler <davidfowl@gmail.com>
2010-11-07 01:25:03 -0800

    Remove PackageSources and associated build steps.

--- PackageSources/SQLCE.EntityFramework/Content/SQLCEEntityFramework.cs.pp
+++
@@ -1,9 +0,0 @@
-using System.Data.Entity.Infrastructure;
-
-namespace $rootnamespace$ {
-    public static class SQLCEEntityFramework {
-        public static void SetConnectionFactory() {
-            Database.DefaultConnectionFactory = new SqlCeConnectionFactory("System.Data.SqlServerCe.4.0");
-        }
-    }
-}
--- PackageSources/readme.txt
+++
@@ -1,3 +0,0 @@
-This is an obsolete location, so please do not try to add packages here!
-
-Instead, follow instructions on http://NuGetpackages.codeplex.com/
