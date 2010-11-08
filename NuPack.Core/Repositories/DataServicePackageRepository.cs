ae972e9e3ec508444a9cf54c7882a7839febb6bc
pranavkm <prkrishn@hotmail.com>
2010-11-08 07:32:40 -0800

    Adding CryptoHashProviderTest

--- NuPack.Core/Repositories/DataServicePackageRepository.cs
+++ NuPack.Core/Repositories/DataServicePackageRepository.cs
@@ -7,7 +7,6 @@ using System.Xml.Linq;
 namespace NuGet {
     public class DataServicePackageRepository : PackageRepositoryBase {
         private readonly DataServiceContext _context;
-        private readonly IHashProvider _hashProvider;
 
         public DataServicePackageRepository(Uri serviceRepository)
             : this(serviceRepository, new CryptoHashProvider()) {
@@ -16,22 +15,31 @@ namespace NuGet {
 
         public DataServicePackageRepository(Uri serviceRoot, IHashProvider hashProvider) {
             _context = new DataServiceContext(serviceRoot);
-            _hashProvider = hashProvider;
+            HashProvider = hashProvider;
 
             _context.SendingRequest += OnSendingRequest;
             _context.ReadingEntity += OnReadingEntity;
             _context.IgnoreMissingProperties = true;
         }
 
-        public override IQueryable<IPackage> GetPackages() {
-            // REVIEW: Is it ok to assume that the package entity set is called packages?
-            return new BatchedDataServiceQuery<DataServicePackage>(_context, "Packages");
-        }
+        public IHashProvider HashProvider { get; set; }
 
         private void OnReadingEntity(object sender, ReadingWritingEntityEventArgs e) {
             var package = (DataServicePackage)e.Entity;
-            // REVIEW: This is the only way (I know) to download the package on demand
             package.InitializeDownloader(DownloadAndVerifyPackage(package));
+        }
+
+        private Func<IPackage> DownloadAndVerifyPackage(DataServicePackage package) {
+            if (!String.IsNullOrEmpty(package.PackageHash)) {
+                byte[] hashBytes = Convert.FromBase64String(package.PackageHash);
+                return () => HttpWebRequestor.DownloadPackage(_context.GetReadStreamUri(package), (data) => HashProvider.VerifyHash(data, hashBytes), 
+                    useCache: true);
+            }
+            else {
+                // REVIEW: This is the only way (I know) to download the package on demand
+                // GetReadStreamUri cannot be evaluated inside of OnReadingEntity. Lazily evaluate it inside DownloadPackage
+                return () => HttpWebRequestor.DownloadPackage(_context.GetReadStreamUri(package));
+            }
         }
 
         private void OnSendingRequest(object sender, SendingRequestEventArgs e) {
@@ -39,15 +47,9 @@ namespace NuGet {
             HttpWebRequestor.InitializeRequest(e.Request);
         }
 
-        private Func<IPackage> DownloadAndVerifyPackage(DataServicePackage package) {
-            Uri downloadUri = _context.GetReadStreamUri(package);
-            if (!String.IsNullOrEmpty(package.PackageHash)) {
-                byte[] hashBytes = Convert.FromBase64String(package.PackageHash);
-                return () => HttpWebRequestor.DownloadPackage(downloadUri, (data) => _hashProvider.VerifyHash(data, hashBytes), useCache: true);
-            }
-            else {
-                return () => HttpWebRequestor.DownloadPackage(downloadUri);
-            }
+        public override IQueryable<IPackage> GetPackages() {
+            // REVIEW: Is it ok to assume that the package entity set is called packages?
+            return new BatchedDataServiceQuery<DataServicePackage>(_context, "Packages");
         }
     }
 }
---
+++ NuPack.Test/CryptoHashProviderTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NuGet.Test {
+    [TestClass]
+    public class CryptoHashProviderTest {
+        [TestMethod]
+        public void DefaultCryptoHashProviderUsesSHA512() {
+            // Arrange
+            byte[] testBytes = Encoding.UTF8.GetBytes("There is no butter knife");
+            string expectedHash = "xy/brd+/mxheBbyBL7i8Oyy62P2ZRteaIkfc4yA8ncH1MYkbDo+XwBcZsOBY2YeaOucrdLJj5odPvozD430w2g==";
+            IHashProvider hashProvider = new CryptoHashProvider();
+
+            // Act
+            byte[] actualHash = hashProvider.CalculateHash(testBytes);
+
+            // Assert
+            CollectionAssert.AreEqual(actualHash, Convert.FromBase64String(expectedHash));
+        }
+
+        [TestMethod]
+        public void CryptoHashProviderReturnsTrueIfHashAreEqual() {
+            // Arrange
+            byte[] testBytes = Encoding.UTF8.GetBytes("There is no butter knife");
+            string expectedHash = "xy/brd+/mxheBbyBL7i8Oyy62P2ZRteaIkfc4yA8ncH1MYkbDo+XwBcZsOBY2YeaOucrdLJj5odPvozD430w2g==";
+            IHashProvider hashProvider = new CryptoHashProvider();
+
+            // Act
+            bool result = hashProvider.VerifyHash(testBytes, Convert.FromBase64String(expectedHash));
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CryptoHashProviderReturnsFalseIfHashValuesAreUnequal() {
+            // Arrange
+            byte[] testBytes = Encoding.UTF8.GetBytes("There is no butter knife");
+            byte[] badBytes = Encoding.UTF8.GetBytes("this is a bad input");             
+            IHashProvider hashProvider = new CryptoHashProvider();
+
+
+            // Act
+            byte[] testHash = hashProvider.CalculateHash(testBytes);
+            byte[] badHash = hashProvider.CalculateHash(badBytes);
+            bool result = hashProvider.VerifyHash(testHash, badHash);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
