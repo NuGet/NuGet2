faf0f743f0e14063096c7843f5a1a41e0001d10c
pranavkm <prkrishn@hotmail.com>
2010-11-08 07:04:59 -0800

    Adding tests for CryptoHashProvider. Refactor DownloadPackage

--- NuPack.Core/Repositories/DataServicePackageRepository.cs
+++ NuPack.Core/Repositories/DataServicePackageRepository.cs
@@ -7,6 +7,7 @@ using System.Xml.Linq;
 namespace NuGet {
     public class DataServicePackageRepository : PackageRepositoryBase {
         private readonly DataServiceContext _context;
+        private readonly IHashProvider _hashProvider;
 
         public DataServicePackageRepository(Uri serviceRepository)
             : this(serviceRepository, new CryptoHashProvider()) {
@@ -15,26 +16,22 @@ namespace NuGet {
 
         public DataServicePackageRepository(Uri serviceRoot, IHashProvider hashProvider) {
             _context = new DataServiceContext(serviceRoot);
-            HashProvider = hashProvider;
+            _hashProvider = hashProvider;
 
             _context.SendingRequest += OnSendingRequest;
             _context.ReadingEntity += OnReadingEntity;
             _context.IgnoreMissingProperties = true;
         }
 
-        public IHashProvider HashProvider { get; set; }
+        public override IQueryable<IPackage> GetPackages() {
+            // REVIEW: Is it ok to assume that the package entity set is called packages?
+            return new BatchedDataServiceQuery<DataServicePackage>(_context, "Packages");
+        }
 
         private void OnReadingEntity(object sender, ReadingWritingEntityEventArgs e) {
             var package = (DataServicePackage)e.Entity;
-            string packageHash = package.PackageHash;
-            if (!String.IsNullOrEmpty(packageHash)) {
-                // REVIEW: This is the only way (I know) to download the package on demand
-                package.InitializeDownloader(() =>
-                    HttpWebRequestor.DownloadPackage(_context.GetReadStreamUri(package), (data) => HashProvider.VerifyHash(data, Convert.FromBase64String(packageHash)), true));
-            }
-            else {
-                package.InitializeDownloader(() => HttpWebRequestor.DownloadPackage(_context.GetReadStreamUri(package)));
-            }
+            // REVIEW: This is the only way (I know) to download the package on demand
+            package.InitializeDownloader(DownloadAndVerifyPackage(package));
         }
 
         private void OnSendingRequest(object sender, SendingRequestEventArgs e) {
@@ -42,9 +39,15 @@ namespace NuGet {
             HttpWebRequestor.InitializeRequest(e.Request);
         }
 
-        public override IQueryable<IPackage> GetPackages() {
-            // REVIEW: Is it ok to assume that the package entity set is called packages?
-            return new BatchedDataServiceQuery<DataServicePackage>(_context, "Packages");
+        private Func<IPackage> DownloadAndVerifyPackage(DataServicePackage package) {
+            Uri downloadUri = _context.GetReadStreamUri(package);
+            if (!String.IsNullOrEmpty(package.PackageHash)) {
+                byte[] hashBytes = Convert.FromBase64String(package.PackageHash);
+                return () => HttpWebRequestor.DownloadPackage(downloadUri, (data) => _hashProvider.VerifyHash(data, hashBytes), useCache: true);
+            }
+            else {
+                return () => HttpWebRequestor.DownloadPackage(downloadUri);
+            }
         }
     }
 }
