2e3fb1b14b1062fbcddeb31a43a0c120ce541d9d
dfowler <davidfowl@gmail.com>
2010-11-09 22:19:44 -0800

    Added unit tests for BindingRedirect resolver.

---
+++ NuPack.Test/BindingRedirectResolverTest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NuGet.Runtime;
+using NuGet.Test.Mocks;
+
+namespace NuGet.Test {
+    [TestClass]
+    public class BindingRedirectResolverTest {
+        [TestMethod]
+        public void GetBindingRedirectsTest() {
+            // A, B, C2, G
+            // A -> C1
+            // B -> G
+            // G -> C2
+            var A = new MockAssembly {
+                Name = "A",
+                Version = new Version("1.0.0.0"),
+                PublicKeyToken = "a34a755ec277222f"
+            };
+
+            var B = new MockAssembly {
+                Name = "B",
+                Version = new Version("1.0.0.0"),
+                PublicKeyToken = "b34a755ec277222f"
+            };
+
+            var C1 = new MockAssembly {
+                Name = "C",
+                Version = new Version("1.0.0.0"),
+                PublicKeyToken = "c34a755ec277222f"
+            };
+
+            var C2 = new MockAssembly {
+                Name = "C",
+                Version = new Version("2.0.0.0"),
+                PublicKeyToken = "c34a755ec277222f"
+            };
+
+            var D = new MockAssembly {
+                Name = "D",
+                Version = new Version("1.0.0.0"),
+                PublicKeyToken = "f34a755ec277222f"
+            };
+
+            var G = new MockAssembly {
+                Name = "G",
+                Version = new Version("1.0.0.0"),
+                PublicKeyToken = "d34a755ec277222f"
+            };
+
+            var assemblies = new[] { A, B, C2, D, G };
+
+            B.References.Add(G);
+            D.References.Add(C2);
+            G.References.Add(C1);
+            A.References.Add(C2);
+
+            // Act
+            var redirectAssemblies = BindingRedirectResolver.GetBindingRedirects(assemblies).ToList();
+
+            // Assert
+            Assert.AreEqual(1, redirectAssemblies.Count);
+            Assert.AreEqual("C", redirectAssemblies[0].Name);
+            Assert.AreEqual("2.0.0.0", redirectAssemblies[0].NewVersion);
+        }
+
+        [TestMethod]
+        public void GetBindingRedirectsOnlyRedirectsStrongNamedAssemblies() {
+            // A, B, C2
+            // A -> C2
+            // B -> C1
+            var A = new MockAssembly {
+                Name = "A",
+                Version = new Version("1.0.0.0"),
+                PublicKeyToken = "a34a755ec277222f"
+            };
+
+            var B = new MockAssembly {
+                Name = "B",
+                Version = new Version("1.0.0.0"),
+                PublicKeyToken = "b34a755ec277222f"
+            };
+
+            var C1 = new MockAssembly {
+                Name = "C",
+                Version = new Version("1.0.0.0")
+            };
+
+            var C2 = new MockAssembly {
+                Name = "C",
+                Version = new Version("2.0.0.0"),
+                PublicKeyToken = "c34a755ec277222f"
+            };
+
+            var assemblies = new[] { A, B, C2 };
+
+            A.References.Add(C2);
+            B.References.Add(C1);
+
+            // Act
+            var redirectAssemblies = BindingRedirectResolver.GetBindingRedirects(assemblies).ToList();
+
+            // Assert
+            Assert.AreEqual(0, redirectAssemblies.Count);
+        }
+    }
+}
---
+++ NuPack.Test/Mocks/MockAssembly.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using NuGet.Runtime;
+
+namespace NuGet.Test.Mocks {
+    public class MockAssembly : IAssembly {
+        public MockAssembly() {
+            References = new List<IAssembly>();
+        }
+
+        public string Name {
+            get;
+            set;
+        }
+
+        public Version Version {
+            get;
+            set;
+        }
+
+        public string PublicKeyToken {
+            get;
+            set;
+        }
+
+        public string Culture {
+            get;
+            set;
+        }
+
+        public List<IAssembly> References {
+            get;
+            private set;
+        }
+
+        public IEnumerable<IAssembly> ReferencedAssemblies {
+            get {
+                return References;
+            }
+        }
+    }
+}
