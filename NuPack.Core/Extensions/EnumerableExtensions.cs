c8887962cd57d867721dbad02b13b0ad5d0ebe08
pranavkm <prkrishn@hotmail.com>
2010-11-13 11:03:19 -0800

    Use SafeQueryable rewriting only when necessary
    --HG--
    extra : rebase_source : d4a4bf6b223808acbb6bc98819e095d9f0c4b214

--- NuPack.Core/Extensions/EnumerableExtensions.cs
+++ NuPack.Core/Extensions/EnumerableExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace NuGet {
     internal static class EnumerableExtensions {
@@ -10,10 +11,30 @@ namespace NuGet {
         /// and expression compilation doesn't work in some cases. See SafeEnumerableQuery for more details.
         /// </summary>
         internal static IQueryable<T> AsSafeQueryable<T>(this IEnumerable<T> source) {
+            return AsSafeQueryable(source, rewriteQuery: IsRewritingRequired());
+        }
+
+        internal static IQueryable<T> AsSafeQueryable<T>(this IEnumerable<T> source, bool rewriteQuery) {
             if (source == null) {
                 throw new ArgumentNullException("source");
             }
-            return new SafeEnumerableQuery<T>(source);
+
+            if (rewriteQuery) {
+                return new SafeEnumerableQuery<T>(source);
+            }
+            // AsQueryable returns the original source if it is already a IQueryable<T>. 
+            return source.AsQueryable();
+        }
+
+        /// <summary>
+        /// Replacing closures with constant values is required only when executing in partial trust and the NuGet assembly is GACed.
+        /// </summary>
+        private static bool IsRewritingRequired() {
+            return IsRewritingRequired(AppDomain.CurrentDomain, typeof(EnumerableExtensions).Assembly);
+        }
+
+        internal static bool IsRewritingRequired(AppDomain appDomain, Assembly assembly) {
+            return !appDomain.IsFullyTrusted && assembly.GlobalAssemblyCache;
         }
     }
 }
---
+++ NuPack.Test/EnumerableExtensionsTest.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NuGet.Test {
+    [TestClass]
+    public class EnumerableExtensionsTest {
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void SafeQueryableThrowsIfSourceIsNull() {
+            // Arrange
+            IEnumerable<int> source = null;
+
+            // Act 
+            ExceptionAssert.ThrowsArgNull(() => source.AsSafeQueryable(), "source");
+        }
+
+        [TestMethod]
+        public void SafeQueryableReturnsOriginalIQueryableWhenNotRewritingQueries() {
+            // Arrange
+            IQueryable<int> source = Enumerable.Range(0, 4).AsQueryable();
+
+            // Act 
+            IQueryable<int> result = source.AsSafeQueryable(rewriteQuery: false);
+
+            // Assert
+            Assert.AreEqual(result, source);
+        }
+
+        [TestMethod]
+        public void SafeQueryableWrapsIEnumerableWhenNotRewritingQueries() {
+            // Arrange
+            IEnumerable<int> source = Enumerable.Range(0, 4);
+
+            // Act 
+            IQueryable<int> result = source.AsSafeQueryable(rewriteQuery: false);
+
+            // Assert
+            Assert.AreEqual(result.GetType(), typeof(EnumerableQuery<int>));
+        }
+
+        [TestMethod]
+        public void SafeQueryableReturnsSafeEnumerableQueryWhenRewriting() {
+            // Arrange
+            IEnumerable<int> source = Enumerable.Range(0, 4);
+
+            // Act 
+            IQueryable<int> result = source.AsSafeQueryable(rewriteQuery: true);
+
+            // Assert
+            Assert.AreEqual(result.GetType(), typeof(SafeEnumerableQuery<int>));
+        }
+
+        [TestMethod]
+        public void IsRewritingRequiredReturnsFalseIfApplicationIsRunningInFullTrust() {
+            // Arrange
+            AppDomain appDomain = GetFullTrustDomain();
+            Assembly gacedAssembly = GetGACedAssembly();
+
+            // Act
+            bool requiresRewrite = EnumerableExtensions.IsRewritingRequired(appDomain, gacedAssembly);
+
+            // Assert
+            Assert.IsFalse(requiresRewrite);
+        }
+
+        [TestMethod]
+        public void IsRewritingRequiredReturnsFalseIfAssemblyIsNotGACed() {
+            // Arrange
+            AppDomain appDomain = GetPartialTrustDomain();
+            Assembly binDeployedAssembly = GetBinDeployedAssembly();
+
+            // Act
+            bool requiresRewrite = EnumerableExtensions.IsRewritingRequired(appDomain, binDeployedAssembly);
+
+            // Assert
+            Assert.IsFalse(requiresRewrite);
+        }
+
+        [TestMethod]
+        public void IsRewritingRequiredReturnsTrueIfAppDomainIsPartiallyTrustedAndAssemblyIsGACed() {
+            // Arrange
+            AppDomain appDomain = GetPartialTrustDomain();
+            Assembly binDeployedAssembly = GetGACedAssembly();
+
+            // Act
+            bool requiresRewrite = EnumerableExtensions.IsRewritingRequired(appDomain, binDeployedAssembly);
+
+            // Assert
+            Assert.IsTrue(requiresRewrite);
+        }
+
+
+        private AppDomain GetFullTrustDomain() {
+            AppDomainSetup setup = new AppDomainSetup { ApplicationBase = TestContext.TestRunDirectory };
+            PermissionSet permissions = new PermissionSet(PermissionState.Unrestricted);
+            AppDomain appDomain = AppDomain.CreateDomain("Full Trust AppDomain", null, setup, permissions);
+
+            Debug.Assert(appDomain.IsFullyTrusted);
+
+            return appDomain;
+        }
+
+        // Based on article "How to host partial trust sandbox"
+        // http://blogs.rev-net.com/ddewinter/2009/05/22/how-to-host-a-partial-trust-sandbox/
+        private AppDomain GetPartialTrustDomain() {
+            AppDomainSetup setup = new AppDomainSetup { ApplicationBase = TestContext.TestRunDirectory };
+            PermissionSet permissions = new PermissionSet(permSet: null);
+            permissions.AddPermission(new SecurityPermission(SecurityPermissionFlag.Execution));
+            permissions.AddPermission(new ReflectionPermission(ReflectionPermissionFlag.RestrictedMemberAccess));
+            AppDomain appDomain = AppDomain.CreateDomain("Partial Trust AppDomain", null, setup, permissions);
+
+            Debug.Assert(!appDomain.IsFullyTrusted);
+
+            return appDomain;
+        }
+
+        private Assembly GetBinDeployedAssembly() {
+            Assembly assembly = this.GetType().Assembly;
+            Debug.Assert(!assembly.GlobalAssemblyCache, "Unit tests should never be GACed");
+
+            return assembly;
+        }
+
+        private static Assembly GetGACedAssembly() {
+            Assembly assembly = typeof(String).Assembly;
+            Debug.Assert(assembly.GlobalAssemblyCache, "mscorlib has to be GACed");
+
+            return assembly;
+        }
+    }
+}
