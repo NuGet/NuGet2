268a96de0db7421abb7c42f9a7f133de1c15b366
pranavkm <prkrishn@hotmail.com>
2010-11-14 11:55:49 -0800

    Changes per Levi's review

--- NuPack.Core/Extensions/EnumerableExtensions.cs
+++ NuPack.Core/Extensions/EnumerableExtensions.cs
@@ -5,13 +5,15 @@ using System.Reflection;
 
 namespace NuGet {
     internal static class EnumerableExtensions {
+        private static readonly bool _isRewritingRequired = IsRewritingRequired();
+
         /// <summary>
         /// The purpose of this method is to mitigate a partial trust issue. We expose
         /// EnumerableQuery (wrapping an enumrable in an IQueryable) throughout the codebase
         /// and expression compilation doesn't work in some cases. See SafeEnumerableQuery for more details.
         /// </summary>
         internal static IQueryable<T> AsSafeQueryable<T>(this IEnumerable<T> source) {
-            return AsSafeQueryable(source, rewriteQuery: IsRewritingRequired());
+            return AsSafeQueryable(source, rewriteQuery: _isRewritingRequired);
         }
 
         internal static IQueryable<T> AsSafeQueryable<T>(this IEnumerable<T> source, bool rewriteQuery) {
@@ -30,11 +32,9 @@ namespace NuGet {
         /// Replacing closures with constant values is required only when executing in partial trust and the NuGet assembly is GACed.
         /// </summary>
         private static bool IsRewritingRequired() {
-            return IsRewritingRequired(AppDomain.CurrentDomain, typeof(EnumerableExtensions).Assembly);
-        }
-
-        internal static bool IsRewritingRequired(AppDomain appDomain, Assembly assembly) {
-            return !appDomain.IsFullyTrusted && assembly.GlobalAssemblyCache;
+            AppDomain appDomain = AppDomain.CurrentDomain;
+            Assembly assembly = typeof(EnumerableExtensions).Assembly; ;
+            return appDomain.IsHomogenous && !appDomain.IsFullyTrusted && assembly.IsFullyTrusted;
         }
     }
 }
