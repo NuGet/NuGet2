87afba01db4af042dc214f3abada81464d61bcf1
dfowler <davidfowl@gmail.com>
2010-12-12 13:21:33 -0800

    Rollback premature optimization.

--- NuPack.Core/Repositories/AggregateRepository.cs
+++ NuPack.Core/Repositories/AggregateRepository.cs
@@ -5,21 +5,15 @@ using System.Linq;
 namespace NuGet {
     public class AggregateRepository : PackageRepositoryBase {
         private readonly IEnumerable<IPackageRepository> _repositories;
-        private readonly IPackageRepository _single;
 
         public AggregateRepository(IEnumerable<IPackageRepository> repositories) {
             if (repositories == null) {
                 throw new ArgumentNullException("repositories");
             }
             _repositories = repositories;
-            _single = _repositories.SingleOrNull();
         }
 
         public override IQueryable<IPackage> GetPackages() {
-            // Small optimization, if there is only one repository then we just delegate to that.
-            if (_single != null) {
-                return _single.GetPackages();
-            }
             return new AggregateQuery<IPackage>(_repositories.Select(r => r.GetPackages()), PackageEqualityComparer.IdAndVersion);
         }
     }
