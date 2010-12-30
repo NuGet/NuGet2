157dd2d49dc9a4dd0961e8a767881e4279b52b7b
dfowler <davidfowl@gmail.com>
2010-12-29 23:45:49 -0800

    Improve functional test run output. - Always run all tests if no specific test specified even if one fails.
    --HG--
    extra : rebase_source : a39170da9570a835dfee3cff0357e6e4e8d29744

--- test/EndToEnd/NuGet.Tests.psm1
+++ test/EndToEnd/NuGet.Tests.psm1
@@ -25,6 +25,7 @@ $msbuildPath = Join-Path $env:windir Microsoft.NET\Framework\v4.0.30319\msbuild
 # TODO: Add the ability to rerun failed tests from the previous run
 
 function global:Run-Test {
+    [CmdletBinding()]
     param(
         [string]$Test
     )
@@ -44,7 +45,7 @@ function global:Run-Test {
     
     $testRunId = New-Guid
     $testRunOutputPath = Join-Path $testOutputPath $testRunId
-    $testRunResultsFile = Join-Path $testRunOutputPath "Results.txt"
+    $testRunResultsFile = Join-Path $testRunOutputPath "Results.html"
     
     # Create the output folder
     mkdir $testRunOutputPath | Out-Null
@@ -53,6 +54,8 @@ function global:Run-Test {
     Get-ChildItem $currentPath -Filter *.ps1 | %{ 
         . $_.FullName $testRunOutputPath $templatePath
     }
+    
+    Write-Verbose "Loading scripts from `"$testPath`""
     
     # Load all of the test scripts
     Get-ChildItem $testPath -Filter *.ps1 | %{ 
@@ -73,6 +76,8 @@ function global:Run-Test {
             throw "The test `"$Test`" doesn't exist"
         } 
     }    
+    
+    $results = @()
     
     try {
         # Run all tests
@@ -102,13 +107,24 @@ function global:Run-Test {
             try {
                 & $_ $context
                 
-                Write-Host -ForegroundColor DarkGreen "Test $name Pass"
-                
-                "$name Pass" >> $testRunResultsFile
+                Write-Host -ForegroundColor DarkGreen "Test $name Passed"
                 
                 if(!$Test) {
                     $dte.Solution.Close()
                 }
+                
+                $results += New-Object PSObject -Property @{ 
+                    Test = $name
+                    Error = $null
+                }
+            }
+            catch {
+                $results += New-Object PSObject -Property @{ 
+                    Test = $name
+                    Error = $_
+                }
+                
+                Write-Host -ForegroundColor Red "$($_.InvocationInfo.InvocationName) Failed: $_"
             }
             finally {            
                 # Cleanup the output from running the generate packages tool
@@ -117,19 +133,136 @@ function global:Run-Test {
             }
         }
     }
-    catch {
-        Write-Host -ForegroundColor Red "Test $name Failed"
-        
-        "$name Failed: $_" >> $testRunResultsFile        
-        throw
-    }
     finally {    
         # Deleting tests
         rm function:\Test*
         
         # Set focus back to powershell
-        $window.SetFocus()
-        
-        "Results were written to $testRunResultsFile"         
+        $window.SetFocus()              
+               
+        Write-TestResults $testRunId $results $testRunResultsFile
     }
+}
+
+function Write-TestResults {
+    param(
+        $TestRunId,
+        $Results,
+        $Path
+    )
+    
+    $resultsTemplate = "<html>
+    <head>
+        <title>
+            Test run {0} results
+        </title>
+        <style>
+            
+        body
+        {{
+            font-family: Trebuchet MS;
+            font-size: 0.80em;
+            color: #000;
+        }}
+
+        a:link, a:visited
+        {{
+            text-decoration: none;
+        }}
+
+        p, ul
+        {{
+            margin-bottom: 20px;
+            line-height: 1.6em;
+        }}
+
+        h1, h2, h3, h4, h5, h6
+        {{
+            font-size: 1.5em;
+            color: #000;
+            font-family: Arial, Helvetica, sans-serif;
+        }}
+
+        h1
+        {{
+            font-size: 1.8em;
+            padding-bottom: 0;
+            margin-bottom: 0;
+        }}
+        h2
+        {{
+            padding: 0 0 10px 0;
+        }}  
+        table
+        {{
+            width: 90%;
+            border-collapse:collapse;
+        }}
+        table td
+        {{
+            padding: 4px;
+            border:1px solid #CCC;
+        }}
+        table th 
+        {{
+            text-align:left;
+            border:1px solid #CCC;
+        }}
+        .Passed 
+        {{
+        }}
+        .Failed
+        {{
+            color:White;
+            background-color:Red;
+            font-weight:bold;
+        }}
+        </style>
+    </head>
+    <body>
+        <h2>Test Run {0} ({1})</h2>
+        <h3>Ran {2} Tests, {3} Passed, {4} Failed</h3>
+        <table>
+            <tr>
+                <th>
+                    Result
+                </th>
+                <th>
+                    Test Name
+                </th>
+                <th>
+                    Error Message
+                </th>
+            </tr>
+            {5}
+            </table>
+    </body>
+</html>";
+
+    $testTemplate = "<tr>
+    <td class=`"{0}`">{0}</td>
+    <td class=`"{0}`">{1}</td>
+    <td class=`"{0}`">{2}</td>
+    </tr>"
+    
+    $pass = 0
+    $fail = 0
+    
+    $rows = $Results | %{   
+        $status = 'Passed'
+        if($_.Error) {
+            $status = 'Failed'
+            $fail++
+        }
+        else {
+            $pass++
+        }
+        
+        [String]::Format($testTemplate, $status, 
+                         [System.Net.WebUtility]::HtmlEncode($_.Test), 
+                         [System.Net.WebUtility]::HtmlEncode($_.Error))
+    }
+
+    [String]::Format($resultsTemplate, $TestRunId, (Split-Path $Path), $Results.Count, $pass, $fail, [String]::Join("", $rows)) | Out-File $Path | Out-Null
+    Write-Host "Ran $($Results.Count) Tests, $pass Passed, $fail Failed. See $Path for more details"
 }
