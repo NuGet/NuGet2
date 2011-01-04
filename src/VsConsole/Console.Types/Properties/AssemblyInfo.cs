a0cd68ecad37e67faa56cdb3cf9dccd961f08b67
dotnetjunky <kinhln@microsoft.com>
2011-01-03 19:01:47 -0800

    Turn on FxCop for Console.Types and set other common settings.

--- src/VsConsole/Console.Types/Properties/AssemblyInfo.cs
+++ src/VsConsole/Console.Types/Properties/AssemblyInfo.cs
@@ -1,36 +1,12 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Console.Types")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Console.Types")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: AssemblyTitle("NuGet.Console.Types")]
+[assembly: AssemblyDescription("Package Manager Powershell Console Types")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b5b74c80-0d1f-43c8-8b01-f3c28bd92a45")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
