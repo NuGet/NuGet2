ee3e2a53d326b1fc309e93b44ac22d291af54ec0
osbornm <matthew@osbornm.com>
2011-01-21 15:12:19 -0800

    forgot to add the base class file to HG...
    --HG--
    extra : rebase_source : 552ae75016e5eb1b1b19ca3abb4adad71d634273

---
+++ src/CommandLine/Commands/Command.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NuGet.Common;
+using System.ComponentModel.Composition;
+
+namespace NuGet.Commands {
+    public abstract class Command : ICommand {
+
+        public List<string> Arguments { get; set; }
+
+        [Import]
+        public IConsole Console { get; set; }
+
+        [Import("HelpCommand")]
+        public HelpCommand HelpCommand { get; set; }
+
+        [Option("help", AltName = "?")]
+        public bool Help { get; set; }
+
+        public void Execute() {
+            if (Help) {
+                HelpCommand.ViewHelpForCommand(GetCurrentCommandName());
+            }
+            else {
+                ExecuteCommand();
+            }
+        }
+
+        public string GetCurrentCommandName() {
+            var CommandAttributes = this.GetType().GetCustomAttributes(typeof(CommandAttribute), true);
+            if (CommandAttributes.Any()) {
+                return ((CommandAttribute)CommandAttributes.First()).CommandName;
+            }
+            return String.Empty;
+        }
+
+        public abstract void ExecuteCommand();
+    }
+}
