8aef5988a6a9bef538abeae4a12bb2cd04ae7af1
osbornm <matthew@osbornm.com>
2011-01-24 11:14:10 -0800

    Removing extra Export that is no longer needed.

--- src/CommandLine/Commands/Command.cs
+++ src/CommandLine/Commands/Command.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.Composition;
 using NuGet.Common;
 
 namespace NuGet.Commands {
-    [Export(typeof(ICommand))]
     public abstract class Command : ICommand {
         public Command() {
             Arguments = new List<string>();
