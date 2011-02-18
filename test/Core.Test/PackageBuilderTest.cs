ce4188f44588677f918eb1b6c5cde320a3f01328
dfowler <davidfowl@gmail.com>
2011-02-17 23:23:05 -0800

    Fixed formatting.

--- test/Core.Test/PackageBuilderTest.cs
+++ test/Core.Test/PackageBuilderTest.cs
@@ -468,8 +468,7 @@ Description is required.");
         }
 
         [TestMethod]
-        public void ReadingManifestWithOldStyleXmlnsDeclaratoinsFromStreamCopiesMetadata()
-        {
+        public void ReadingManifestWithOldStyleXmlnsDeclaratoinsFromStreamCopiesMetadata() {
             // Arrange
             string spec = @"<?xml version=""1.0""?>
 <package xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
