02309b23a8214a735559ba9a456b8a7caa9c5f6e
dfowler <davidfowl@gmail.com>
2011-02-19 11:27:12 -0800

    Make PowerShellScripts class static per review.

--- src/VisualStudio/PowershellScripts.cs
+++ src/VisualStudio/PowershellScripts.cs
@@ -1,5 +1,5 @@
 ﻿namespace NuGet.VisualStudio {
-    public class PowerShellScripts {
+    public static class PowerShellScripts {
         public static readonly string Install = "install.ps1";
         public static readonly string Uninstall = "uninstall.ps1";
         public static readonly string Init = "init.ps1";
