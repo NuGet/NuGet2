2eeca5104b6aef60c5a638f55726f12e209cd73d
dfowler <davidfowl@gmail.com>
2011-02-19 12:44:50 -0800

    Ignore debug constants.

--- src/VsConsole/PowerShellHost/DebugConstants.cs
+++
@@ -1,9 +0,0 @@
-#if DEBUG
-using System.IO;
-
-namespace NuGetConsole.Host.PowerShell {
-    public class DebugConstants {
-        internal static string TestModulePath = Path.Combine(@"ENLISTMENT_ROOT", @"test\EndToEnd\NuGet.Tests.psd1");
-    }
-}
-#endif
