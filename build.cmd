9f1ccf9399938213da74fde5b55a6e5cb37b09a1
pranavkm <prkrishn@hotmail.com>
2011-02-23 22:46:47 -0800

    Making build.cmd build in parallel

--- build.cmd
+++ build.cmd
@@ -1,5 +1,6 @@
+@echo Off
 set config=%1
 if "%config%" == "" (
    set config=debug
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%"
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%" /m
