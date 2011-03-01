813d20599ebf426b37f0069684693b4b4a86e9c7
dotnetjunky <kinhln@microsoft.com>
2011-02-28 18:56:10 -0800

    [Package Explorer] Add shortcut to open the Select package dialog.

---
+++ Tools/NuGet/NuGetPackageExplorer/PackageExplorer/Commands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace PackageExplorer
+{
+    public static class Commands
+    {
+        public static readonly RoutedUICommand OpenFromFeedCommand = 
+            new RoutedUICommand("Open from feed...", "OpenFromFeed", typeof(Commands), 
+                new InputGestureCollection() { new KeyGesture(Key.G, ModifierKeys.Control) });
+
+    }
+}
