682675a24b94c006262c8d718e0a3f817e0cf892
dotnetjunky <kinhln@microsoft.com>
2011-03-04 16:05:50 -0800

    Package icon is missing when the gallery sends down relative path. We ignore them and show the nuget icon instead. Work items: 768

---
+++ src/Dialog/PackageManagerUI/Converters/FixUrlConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace NuGet.Dialog.PackageManagerUI {
+    public class FixUrlConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            Uri source = (Uri)value;
+            if (source == null || !source.IsAbsoluteUri) {
+                // the nuget gallery has a bug where it sends down relative path. We ignore them.
+                return null;
+            }
+
+            return source;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
