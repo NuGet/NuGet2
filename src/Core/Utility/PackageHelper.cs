b3eec11eab2c2d6fe48f03f50dc0dfa6075bccfe
dfowler <davidfowl@gmail.com>
2011-03-09 00:35:18 -0800

    Added comments.
    --HG--
    branch : 1.2

--- src/Core/Utility/PackageHelper.cs
+++ src/Core/Utility/PackageHelper.cs
@@ -12,11 +12,13 @@ namespace NuGet {
 
             IPackage package = null;
 
+            // If we're looking for an exact version of a package then try local first
             if (version != null) {
                 package = localRepository.FindPackage(packageId, version);
             }
 
             if (package == null) {
+                // Try to find it in the source (regardless of version)
                 package = sourceRepository.FindPackage(packageId, version: version);
 
                 // If we already have this package installed, use the local copy so we don't 
@@ -26,6 +28,7 @@ namespace NuGet {
                 }
             }
 
+            // We still didn't find it so throw
             if (package == null) {
                 throw new InvalidOperationException(
                     String.Format(CultureInfo.CurrentCulture,
