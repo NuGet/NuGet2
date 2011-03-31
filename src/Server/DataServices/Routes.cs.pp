81c339a694bffe8fc6951afc57ed19fb6af0f1e2
dfowler <davidfowl@gmail.com>
2011-03-30 21:07:19 -0700

    Added generation of NuGet.Core package to build Fix NuGet.Server package
    --HG--
    branch : 1.2oob
    extra : rebase_source : 0bfb8f357bde306da2e742d15f49a3999fc51be6

---
+++ src/Server/DataServices/Routes.cs.pp
@@ -0,0 +1,20 @@
+﻿using System.Data.Services;
+using System.ServiceModel.Activation;
+using System.Web.Routing;
+using NuGet.Server.DataServices;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.NuGetRoutes), "Start")]
+
+namespace $rootnamespace$ {
+    public static class NuGetRoutes {
+        public static void Start() {
+            // The default route is http://{root}/nuget/Packages
+            
+            var factory = new DataServiceHostFactory();
+            var serviceRoute = new ServiceRoute("nuget", factory, typeof(Packages));
+            serviceRoute.Defaults = new RouteValueDictionary { { "serviceType", "odata" } };
+            serviceRoute.Constraints = new RouteValueDictionary { { "serviceType", "odata" } };
+            RouteTable.Routes.Add("nuget", serviceRoute); 
+        }
+    }
+}
