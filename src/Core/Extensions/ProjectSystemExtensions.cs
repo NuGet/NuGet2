5e53c759f411a3bd956fa74fedc93b3641c632a6
dfowler <davidfowl@gmail.com>
2011-04-07 09:47:31 -0700

    Removed unused using.
    --HG--
    extra : rebase_source : 577d411159a64a5400ecc94f478c377b7f7afd61

--- src/Core/Extensions/ProjectSystemExtensions.cs
+++ src/Core/Extensions/ProjectSystemExtensions.cs
@@ -5,7 +5,6 @@ using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Runtime.Versioning;
 using NuGet.Resources;
 
 namespace NuGet {
