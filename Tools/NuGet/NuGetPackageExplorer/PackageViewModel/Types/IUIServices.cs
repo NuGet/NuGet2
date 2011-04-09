03ab1c4828cf820748b31f85fa30fa2893c4c70f
dotnetjunky <kinhln@microsoft.com>
2011-04-09 14:56:19 -0700

    [Package Explrer] Fix FxCop errors

--- Tools/NuGet/NuGetPackageExplorer/PackageViewModel/Types/IUIServices.cs
+++ Tools/NuGet/NuGetPackageExplorer/PackageViewModel/Types/IUIServices.cs
@@ -1,8 +1,11 @@
 ﻿
 namespace PackageExplorerViewModel.Types {
     public interface IUIServices {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1021:AvoidOutParameters", MessageId = "3#")]
         bool OpenSaveFileDialog(string title, string defaultFileName, string filter, out string selectedFilePath);
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1021:AvoidOutParameters", MessageId = "2#")]
         bool OpenFileDialog(string title, string filter, out string selectedFileName);
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1021:AvoidOutParameters", MessageId = "2#")]
         bool OpenMultipleFilesDialog(string title, string filter, out string[] selectedFileNames);
     }
 }
