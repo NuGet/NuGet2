31cb67c8485f6d12d7fc0e4dad51495ef677eac2
dotnetjunky <kinhln@microsoft.com>
2011-04-13 23:54:30 -0700

    [Package Explorer] Automate creating ClickOnce publish zip file.

--- Tools/NuGet/NuGetPackageExplorer/build.cmd
+++ Tools/NuGet/NuGetPackageExplorer/build.cmd
@@ -3,4 +3,4 @@ set config=%1
 if "%config%" == "" (
    set config=debug
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild NuGetPackageExplorer.sln /p:Configuration="%config%" /p:EnableCodeAnalysis=true
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build\build.proj /p:Configuration="%config%"
