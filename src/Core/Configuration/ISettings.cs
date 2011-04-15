afd7643a9ed3fe67d4aefdee9804d1ff067a8391
osbornm <matthew@osbornm.com>
2011-04-15 14:36:19 -0700

    Suppress FxCop warnings about Generic type nesting in ISettings interface.

--- src/Core/Configuration/ISettings.cs
+++ src/Core/Configuration/ISettings.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace NuGet {
     public interface ISettings {
         string GetValue(string section, string key);
+        [SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "This is the best fit for this internal class")]
         ICollection<KeyValuePair<string, string>> GetValues(string section);
         void SetValue(string section, string key, string value);
+        [SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "This is the best fit for this internal class")]
         void SetValues(string section, ICollection<KeyValuePair<string, string>> values);
         bool DeleteValue(string section, string key);
         bool DeleteSection(string section);
