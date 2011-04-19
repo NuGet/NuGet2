eb270208c9a29d540e65f7c2f05f4126af668691
dfowler <davidfowl@gmail.com>
2011-04-18 23:49:49 -0700

    Change version to 1.4

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
 
 [assembly: NeutralResourcesLanguage("en-US")]
