f6ff26b3c72446e1229cd4a45c93f7966cced798
dfowler <davidfowl@gmail.com>
2011-04-19 12:22:06 -0700

    Added a bootstrapper for NuGet.exe
    --HG--
    branch : 1.3

---
+++ src/Bootstrapper/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+[assembly: AssemblyTitle("NuGet")]
+[assembly: AssemblyDescription("NuGet Command Line Bootstrapper")]
+[assembly: AssemblyCompany("Outercurve Foundation")]
+[assembly: AssemblyProduct("NuGet")]
+[assembly: AssemblyCopyright("\x00a9 Outercurve Foundation. All rights reserved.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+
+[assembly: NeutralResourcesLanguage("en-US")]
