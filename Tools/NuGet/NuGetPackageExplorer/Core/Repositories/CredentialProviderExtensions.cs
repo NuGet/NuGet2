8c3711de8273f66d839c0199f1dab288c7e28add
dotnetjunky <kinhln@microsoft.com>
2011-04-20 16:42:13 -0700

    [Package Explorer] remove a method out of ICredialProvider and make it an extension method.
    --HG--
    branch : 1.3

---
+++ Tools/NuGet/NuGetPackageExplorer/Core/Repositories/CredentialProviderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace NuGet {
+    public static class CredentialProviderExtensions {
+        public static bool HasCredentials(this ICredentialProvider provider, Uri uri) {
+            ICredentials[] credentials = provider.GetCredentials(uri);
+            return credentials != null && credentials.Count() > 0;
+        }
+    }
+}
