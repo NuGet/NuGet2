f5d7ced84cd66a01fffd0cf2f55dbb5580e262af
dfowler <davidfowl@gmail.com>
2011-04-25 21:51:36 -0700

    Refactored interface extraction logic.
    --HG--
    branch : 1.3oob
    extra : rebase_source : 346f25cdb2bfc72a6c886c3e6050c08fc2f9a4dc

--- src/CommandLine/Common/CommandLineUtility.cs
+++ src/CommandLine/Common/CommandLineUtility.cs
@@ -62,11 +62,15 @@ namespace NuGet {
         }
 
         public static Type GetGenericCollectionType(Type type) {
-            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(ICollection<>)) {
+            return GetInterfaceType(type, typeof(ICollection<>));
+        }
+
+        private static Type GetInterfaceType(Type type, Type interfaceType) {
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == interfaceType) {
                 return type;
             }
             return (from t in type.GetInterfaces()
-                    where t.IsGenericType && t.GetGenericTypeDefinition() == typeof(ICollection<>)
+                    where t.IsGenericType && t.GetGenericTypeDefinition() == interfaceType
                     select t).SingleOrDefault();
         }
 
