6322b85fd9efa4ca9fac437ef3bc867ac8fc4df5
dfowler <davidfowl@gmail.com>
2011-06-05 22:08:51 -0700

    Bump version to 1.5

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
 
 [assembly: NeutralResourcesLanguage("en-US")]
