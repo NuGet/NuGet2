4413bc6df66c948d9224bf7cde3d9aa09071d85b
mabster <none@none>
2011-06-10 23:34:01 +1000

    Adhere to coding conventions

--- src/Core/Utility/MemoryCache.cs
+++ src/Core/Utility/MemoryCache.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Concurrent;
 
-namespace NuGet
-{
-    internal sealed class MemoryCache
-    {
-        private MemoryCache()
-        {
+namespace NuGet {
+    internal sealed class MemoryCache {
+        private MemoryCache() {
         }
 
-        internal static MemoryCache Default
-        {
+        internal static MemoryCache Default {
             get
             {
                 return InternalMemoryCache.Instance;
@@ -19,33 +15,27 @@ namespace NuGet
 
         private ConcurrentDictionary<string, CacheItem> _cache = new ConcurrentDictionary<string, CacheItem>();
 
-        internal T GetOrAdd<T>(string cacheKey, Func<T> factory, TimeSpan slidingExpiration) where T : class
-        {
+        internal T GetOrAdd<T>(string cacheKey, Func<T> factory, TimeSpan slidingExpiration) where T : class {
             CacheItem result;
-            if (!_cache.TryGetValue(cacheKey, out result) || result.Expiry < DateTime.Now)
-            {
+            if (!_cache.TryGetValue(cacheKey, out result) || result.Expiry < DateTime.Now) {
                 result = new CacheItem { Item = factory(), Expiry = DateTime.Now.Add(slidingExpiration) };
                 _cache[cacheKey] = result;
             }
             return (T)result.Item;
         }
 
-        internal void Remove(string cacheKey)
-        {
+        internal void Remove(string cacheKey) {
             CacheItem item;
             _cache.TryRemove(cacheKey, out item);
         }
 
-        private class CacheItem
-        {
+        private class CacheItem {
             public object Item { get; set; }
             public DateTime Expiry { get; set; }
         }
 
-        private class InternalMemoryCache
-        {
-            static InternalMemoryCache()
-            {
+        private class InternalMemoryCache {
+            static InternalMemoryCache() {
             }
 
             internal static MemoryCache Instance = new MemoryCache();
