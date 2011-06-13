526acae0799ec23eabffa8ba3556ac987c5667fd
ilyalozovyy <none@none>
2011-06-13 12:32:30 -0400

    [NuGet.exe] Updated input parameter validation logic to throw exceptions instead of Console.WriteError as per dev review.
    --HG--
    branch : 1.4oob

--- src/CommandLine/Commands/AddSourceCommand.cs
+++ src/CommandLine/Commands/AddSourceCommand.cs
@@ -25,31 +25,26 @@ namespace NuGet.Commands {
                 return;
             }
             if (String.Equals(sourceName, NuGetResources.ReservedPackageNameAll)) {
-                Console.WriteError(NuGetResources.AddSourceCommandAllNameIsReserved);
-                return;
+                throw new CommandLineException(NuGetResources.AddSourceCommandAllNameIsReserved);
             }
             string source = Arguments[1];
             if (String.IsNullOrWhiteSpace(source)) {
-                Console.WriteError(NuGetResources.AddSourceCommandSourceRequired);
-                return;
+                throw new CommandLineException(NuGetResources.AddSourceCommandSourceRequired);
             }
 
             // Make sure that the Source given is a valid one.
             if (!PathValidator.IsValidSource(source)) {
-                Console.WriteError(NuGetResources.AddSourceCommandInvalidSource);
-                return;
+                throw new CommandLineException(NuGetResources.AddSourceCommandInvalidSource);
             }
             // Check to see if we already have a registered source with the same name or source
             var sourceList = SourceProvider.LoadPackageSources().ToList();
             bool hasName = sourceList.Any(ps => String.Equals(sourceName, ps.Name, StringComparison.OrdinalIgnoreCase));
             if (hasName) {
-                Console.WriteError(NuGetResources.AddSourceCommandUniqueName);
-                return;
+                throw new CommandLineException(NuGetResources.AddSourceCommandUniqueName);
             }
             bool hasSource = sourceList.Any(ps => String.Equals(source, ps.Source, StringComparison.OrdinalIgnoreCase));
             if (hasSource) {
-                Console.WriteError(NuGetResources.AddSourceCommandUniqueSource);
-                return;                
+               throw new CommandLineException(NuGetResources.AddSourceCommandUniqueSource);
             }
 
             var newPackageSource = new PackageSource(source, sourceName);
--- src/CommandLine/Commands/RemoveSourceCommand.cs
+++ src/CommandLine/Commands/RemoveSourceCommand.cs
@@ -21,15 +21,13 @@ namespace NuGet.Commands {
         public override void ExecuteCommand() {
             string sourceName = Arguments[0];
             if (String.IsNullOrWhiteSpace(sourceName)) {
-                Console.WriteError(NuGetResources.NameRequired);
-                return;
+                throw new CommandLineException(NuGetResources.NameRequired);
             }
             // Check to see if we already have a registered source with the same name or source
             var sourceList = SourceProvider.LoadPackageSources().ToList();
             var existingSource = sourceList.Where(ps => String.Equals(sourceName, ps.Name, StringComparison.OrdinalIgnoreCase)).ToList();
             if (!existingSource.Any()) {
-                Console.WriteError(NuGetResources.RemoveSourceCommandNoMatchingSourcesFound, sourceName);
-                return;
+                throw new CommandLineException(NuGetResources.RemoveSourceCommandNoMatchingSourcesFound, sourceName);
             }
 
             existingSource.ForEach(source => sourceList.Remove(source));
