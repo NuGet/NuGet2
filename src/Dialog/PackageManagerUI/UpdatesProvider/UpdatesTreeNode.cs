ec28e0f15b9e595893253397d83f54de5720c107
dfowler <davidfowl@gmail.com>
2011-06-13 23:33:17 -0700

    Added comment per review.

--- src/Dialog/PackageManagerUI/UpdatesProvider/UpdatesTreeNode.cs
+++ src/Dialog/PackageManagerUI/UpdatesProvider/UpdatesTreeNode.cs
@@ -21,6 +21,8 @@ namespace NuGet.Dialog.Providers {
         }
 
         public override IQueryable<IPackage> GetPackages() {
+            // We need to call ToList() here so that we don't evaluate the enumerable twice
+            // when trying to count it.
             return Repository.GetUpdates(_localRepository.GetPackages())
                              .ToList()
                              .AsQueryable();
