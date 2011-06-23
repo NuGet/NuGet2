85748e613731f553e3a4dc2cbd7eb52c53c28160
pranavkm <prkrishn@hotmail.com>
2011-06-23 12:28:33 -0700

    Updating RouteMagic version to 1.0 in NuGet.Server
    --HG--
    branch : 1.4oob
    rename : packages/RouteMagic.0.2.2.2/content/license.txt => packages/RouteMagic.1.0/content/license.txt
    rename : packages/RouteMagic.0.2.2.2/content/license.txt => src/Server/license.txt

--- src/Server/DataServices/Routes.cs
+++ src/Server/DataServices/Routes.cs
@@ -18,17 +18,17 @@ namespace NuGet.Server {
             // Route to create a new package
             routes.MapDelegate("CreatePackage",
                                "PackageFiles/{apiKey}/nupkg",
-                               context => CreatePackageService().CreatePackage(context));
+                               requestContext => CreatePackageService().CreatePackage(requestContext.HttpContext));
 
             // Route to publish a package
             routes.MapDelegate("PublishPackage",
                                "PublishedPackages/Publish",
-                               context => CreatePackageService().PublishPackage(context));
+                               requestContext => CreatePackageService().PublishPackage(requestContext.HttpContext));
 
             // Route to delete packages
             routes.MapDelegate("DeletePackage",
                                "Packages/{apiKey}/{packageId}/{version}",
-                               context => CreatePackageService().DeletePackage(context));
+                               requestContext => CreatePackageService().DeletePackage(requestContext.HttpContext));
 
 #if DEBUG
             // The default route is http://{root}/nuget/Packages
