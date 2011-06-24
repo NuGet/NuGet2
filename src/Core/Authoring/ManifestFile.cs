0fb5d53962cc9e9284304c25403ef09bd8e83a92
pranavkm <prkrishn@hotmail.com>
2011-06-24 11:10:04 -0700

    Bug 1167: nuget.exe, improve handling of "<file target=...>" error cases in .nuspec
    Work Item: 1167
    
    --HG--
    extra : rebase_source : f2ae1cb2db379cf373112a1ab6ccfb8ca3ccc746

--- src/Core/Authoring/ManifestFile.cs
+++ src/Core/Authoring/ManifestFile.cs
@@ -1,15 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Xml.Serialization;
 using NuGet.Resources;
 
 namespace NuGet {
     [XmlType("file")]
     public class ManifestFile {
+        private static readonly char[] _invalidPathCharacters = Path.GetInvalidPathChars();
+        private static readonly IEnumerable<char> _invalidPathCharactersExceptWildCards = _invalidPathCharacters.Except(new[] { '*', '?' });
+
         [Required(ErrorMessageResourceType = typeof(NuGetResources), ErrorMessageResourceName = "Manifest_RequiredMetadataMissing")]
         [XmlAttribute("src")]
         public string Source { get; set; }
 
         [XmlAttribute("target")]
         public string Target { get; set; }
-    }
-}
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) {
+            if (!String.IsNullOrEmpty(Source) && Source.Any(c => _invalidPathCharactersExceptWildCards.Contains(c))) {
+                yield return new ValidationResult(String.Format(CultureInfo.CurrentCulture, NuGetResources.Manifest_SourceContainsInvalidCharacters, Source), new[] { "Source" });
+            }
+            
+            if (!String.IsNullOrEmpty(Target) && Target.Any(c => _invalidPathCharacters.Contains(c))) {
+                yield return new ValidationResult(String.Format(CultureInfo.CurrentCulture, NuGetResources.Manifest_TargetContainsInvalidCharacters, Target), new[] { "Target" });
+            }
+        }
+     }
+ }
---
+++ test/Core.Test/ManifestFileTest.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NuGet.Test {
+    [TestClass]
+    public class ManifestFileTest {
+        [TestMethod]
+        public void ManifestFileReturnsNoValidationResultsIfSourceAndTargetPathAreValid() {
+            // Arrange
+            var manifestFile = new ManifestFile { Source = @"bin\release\MyLib.dll", Target = @"lib" };
+            var validationContext = new ValidationContext(new object(), null, null);
+
+            // Act
+            var result = manifestFile.Validate(validationContext);
+
+            // Assert
+            Assert.IsFalse(result.Any());
+        }
+
+        [TestMethod]
+        public void ManifestFileReturnsNoValidationResultIfSourceContainsWildCardCharacters() {
+            // Arrange
+            var manifestFile = new ManifestFile { Source = @"b?n\**\*.dll", Target = @"lib" };
+            var validationContext = new ValidationContext(new object(), null, null);
+
+            // Act
+            var result = manifestFile.Validate(validationContext);
+
+            // Assert
+            Assert.IsFalse(result.Any());
+        }
+
+        [TestMethod]
+        public void ManifestFileReturnsValidationResultIfSourceContainsInvalidCharacters() {
+            // Arrange
+            var manifestFile = new ManifestFile { Source = @"bin\\|\\*.dll", Target = @"lib" };
+            var validationContext = new ValidationContext(new object(), null, null);
+
+            // Act
+            var result = manifestFile.Validate(validationContext).ToList();
+
+            // Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(@"Source path ""bin\\|\\*.dll"" contains invalid characters.", result.Single().ErrorMessage);
+        }
+
+        [TestMethod]
+        public void ManifestFileReturnsValidationResultIfTargetContainsInvalidCharacters() {
+            // Arrange
+            var manifestFile = new ManifestFile { Source = @"bin\\**\\*.dll", Target = @"lib\\|\\net40" };
+            var validationContext = new ValidationContext(new object(), null, null);
+
+            // Act
+            var result = manifestFile.Validate(validationContext).ToList();
+
+            // Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(@"Target path ""lib\\|\\net40"" contains invalid characters.", result.Single().ErrorMessage);
+        }
+
+        [TestMethod]
+        public void ManifestFileReturnsValidationResultsIfSourceAndTargetContainsInvalidCharacters() {
+            // Arrange
+            var manifestFile = new ManifestFile { Source = @"bin|\\**\\*.dll", Target = @"lib\\|\\net40" };
+            var validationContext = new ValidationContext(new object(), null, null);
+
+            // Act
+            var result = manifestFile.Validate(validationContext).ToList();
+
+            // Assert
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(@"Source path ""bin|\\**\\*.dll"" contains invalid characters.", result.First().ErrorMessage);
+            Assert.AreEqual(@"Target path ""lib\\|\\net40"" contains invalid characters.", result.Last().ErrorMessage);
+        }
+    }
+}
