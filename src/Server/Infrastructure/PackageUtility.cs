14ffdf6ed28d9e89224f41f754dd2a300f467198
mgrosperrin <none@none>
2011-08-04 11:54:24 +0200

    Use the AppSettings 'NuGetPackagePath' if exists and the default ~/Packages otherwise

--- src/Server/Infrastructure/PackageUtility.cs
+++ src/Server/Infrastructure/PackageUtility.cs
@@ -1,16 +1,38 @@
 using System;
 using System.Web;
 using System.Web.Hosting;
+using System.Configuration;
 
-namespace NuGet.Server.Infrastructure {
-    public class PackageUtility {
-        internal static string PackagePhysicalPath = HostingEnvironment.MapPath("~/Packages");
+namespace NuGet.Server.Infrastructure
+{
+    public class PackageUtility
+    {
 
-        public static Uri GetPackageUrl(string path, Uri baseUri) {
+        internal static string PackagePhysicalPath;
+        private static string DefaultPackagePhysicalPath = HostingEnvironment.MapPath("~/Packages");
+
+
+        static PackageUtility()
+        {
+            string packagePath = ConfigurationManager.AppSettings["NuGetPackagePath"];
+            if (string.IsNullOrEmpty(packagePath))
+            {
+                PackagePhysicalPath = DefaultPackagePhysicalPath;
+            }
+            else
+            {
+                PackagePhysicalPath = packagePath;
+            }
+        }
+
+
+        public static Uri GetPackageUrl(string path, Uri baseUri)
+        {
             return new Uri(baseUri, GetPackageDownloadUrl(path));
         }
 
-        private static string GetPackageDownloadUrl(string path) {
+        private static string GetPackageDownloadUrl(string path)
+        {
             return VirtualPathUtility.ToAbsolute("~/Packages/" + path);
         }
     }
