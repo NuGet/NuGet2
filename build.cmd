c40beeca96e4e65e30911a8fe40cde182604aed6
pranavkm <prkrishn@hotmail.com>
2011-09-20 11:32:29 -0700

    Updating Build.proj to build tests in parallel. Changing build verbosity to minimal
    --HG--
    extra : rebase_source : be8ef271917911fdf3bf8adcaa85dcc32cf509f1

--- build.cmd
+++ build.cmd
@@ -3,4 +3,11 @@ set config=%1
 if "%config%" == "" (
    set config=debug
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%" /m
+pushd %~dp0
+
+if exist bin goto build
+mkdir bin
+
+:Build
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
