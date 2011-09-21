215bfd37f37f3efc264da003aa445c13875e1ef0
pranavkm <prkrishn@hotmail.com>
2011-09-20 19:29:20 -0700

    Output log file in the root directory to avoid warning when building

--- build.cmd
+++ build.cmd
@@ -3,11 +3,5 @@ set config=%1
 if "%config%" == "" (
    set config=debug
 )
-pushd %~dp0
 
-if exist bin goto build
-mkdir bin
-
-:Build
-
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal
