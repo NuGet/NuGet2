3370d633b75f855d993e5c60672baffc7f4a495a
dotnetjunky <kinhln@microsoft.com>
2011-09-26 13:45:09 -0700

    Add PackageIssueLevel enum and the corresponding property to PackageIssue class.
    --HG--
    extra : rebase_source : 796d4f7a619c9eb857b69f391be8821a2953efa2

--- src/Core/Analysis/PackageIssue.cs
+++ src/Core/Analysis/PackageIssue.cs
@@ -3,7 +3,12 @@ using Microsoft.Internal.Web.Utils;
 
 namespace NuGet {
     public class PackageIssue {
-        public PackageIssue(string title, string description, string solution) {
+
+        public PackageIssue(string title, string description, string solution) :
+            this(title, description, solution, PackageIssueLevel.Warning) {
+        }
+
+        public PackageIssue(string title, string description, string solution, PackageIssueLevel level) {
             if (String.IsNullOrEmpty(title)) {
                 throw new ArgumentException(CommonResources.Argument_Cannot_Be_Null_Or_Empty, "title");
             }
@@ -15,8 +20,10 @@ namespace NuGet {
             Title = title;
             Description = description;
             Solution = solution;
+            Level = level;
         }
 
+        public PackageIssueLevel Level { get; private set; }
         public string Title { get; private set; }
         public string Description { get; private set; }
         public string Solution { get; private set; }
---
+++ src/Core/Analysis/PackageIssueLevel.cs
@@ -0,0 +1,7 @@
+﻿
+namespace NuGet {
+    public enum PackageIssueLevel {
+        Warning = 0,
+        Error = 1,
+    }
+}
