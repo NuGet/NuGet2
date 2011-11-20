5be5ab90180abb1f01418dc96f7b7021fc3ef030
dotnetjunky <kinhln@microsoft.com>
2011-11-20 07:08:54 -0800

    Add a safety check for when Authors is null, which shouldn't happen in the first place. Work items: 1727, 1728
    --HG--
    branch : 1.6

--- src/DialogServices/PackageManagerUI/Converters/StringCollectionsToStringConverter.cs
+++ src/DialogServices/PackageManagerUI/Converters/StringCollectionsToStringConverter.cs
@@ -4,21 +4,21 @@ using System.Windows.Data;
 
 namespace NuGet.Dialog.PackageManagerUI
 {
-
     public class StringCollectionsToStringConverter : IValueConverter
     {
-
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             if (targetType == typeof(string))
             {
-
                 string stringValue = value as string;
                 if (stringValue != null)
                 {
                     return stringValue;
                 }
-                else
+                else if (value == null)
+                {
+                    return String.Empty;
+                }
                 {
                     IEnumerable<string> parts = (IEnumerable<string>)value;
                     return String.Join(", ", parts);
