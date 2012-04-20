9517bdde369d9503563447250a2b9b52ed32c327
dotnetjunky <kinhln@microsoft.com>
2012-04-20 16:36:38 -0700

    Make the dialogs look nice in both light theme and dark theme in VS11.
    --HG--
    branch : 1.8

---
+++ src/DialogServices/UIResources.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using Microsoft.VisualStudio.Shell;
+using NuGet.VisualStudio;
+
+namespace NuGet.Dialog 
+{
+    public static class UIResources 
+    {
+        private static object _windowBackgroundBrushKey;
+        private static object _contentBackgroundBrushKey;
+
+        /// <summary>
+        /// This key is used as the background for our dialogs.
+        /// </summary>
+        public static object WindowBackgroundBrushKey 
+        {
+            get
+            {
+                if (VsVersionHelper.IsVisualStudio2010)
+                {
+
+                    return VsBrushes.NewProjectBackgroundKey;
+                }
+                else
+                {
+                    if (_windowBackgroundBrushKey == null)
+                    {
+                        _windowBackgroundBrushKey = GetObject(
+                            "Microsoft.VisualStudio.ExtensionsExplorer.UI.ColorResources, Microsoft.VisualStudio.ExtensionsExplorer.UI", // Class Name, Assembly Name
+                            "BackgroundBrushKey");  // Property Name
+                    }
+
+                    return _windowBackgroundBrushKey;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This key is used for the background of the disclaimer text.
+        /// </summary>
+        public static object ContentBackgroundBrushKey
+        {
+            get
+            {
+                if (VsVersionHelper.IsVisualStudio2010)
+                {
+                    return VsBrushes.WindowKey;
+                }
+                else
+                {
+                    if (_contentBackgroundBrushKey == null)
+                    {
+                        _contentBackgroundBrushKey = GetObject(
+                            "Microsoft.VisualStudio.ExtensionsExplorer.UI.ColorResources, Microsoft.VisualStudio.ExtensionsExplorer.UI", // Class Name, Assembly Name
+                            "WonderbarBrushKey");   // Property Name
+                    }
+
+                    return _contentBackgroundBrushKey;
+                }
+            }
+        }
+
+        private static object GetObject(string fullClassName, string property)
+        {
+            Type type = Type.GetType(fullClassName);
+            if (type == null)
+            {
+                return null;
+            }
+            PropertyInfo propertyInfo = type.GetProperty(property, BindingFlags.Public | BindingFlags.Static);
+            return propertyInfo != null ? propertyInfo.GetValue(null, null) : null;
+        }
+    }
+}
