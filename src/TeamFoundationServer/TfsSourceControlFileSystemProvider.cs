3a8fff390743de151c5a07fd6f2f5a3f679b9cd4
dotnetjunky <me@yahoo.com>
2012-05-30 15:13:17 -0700

    Avoid loading both TeamFoundationServer.dll and TeamFoundationServer10.dll assemblies. Work items: 2226

--- src/TeamFoundationServer/TfsSourceControlFileSystemProvider.cs
+++ src/TeamFoundationServer/TfsSourceControlFileSystemProvider.cs
@@ -1,5 +1,4 @@
 using System;
-using System.ComponentModel.Composition;
 using EnvDTE80;
 using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.VersionControl.Client;
@@ -7,14 +6,13 @@ using NuGet.VisualStudio;
 
 namespace NuGet.TeamFoundationServer
 {
-    [Export(typeof(ISourceControlFileSystemProvider))]
     public class TfsSourceControlFileSystemProvider : ISourceControlFileSystemProvider
     {
         private const string TfsProviderName = "{4CA58AB2-18FA-4F8D-95D4-32DDF27D184C}";
 
         public ISourceControlFileSystem GetFileSystem(string path, SourceControlBindings binding)
         {
-            // Return null if this this binding isn't for us then return null
+            // Return null if this binding isn't for us then return null
             if ((ProviderConstants.SupportedVsVersion != VsVersionHelper.VsMajorVersion) ||
                 String.IsNullOrEmpty(binding.ProviderName) ||
                 !binding.ProviderName.Equals(TfsProviderName, StringComparison.OrdinalIgnoreCase))
---
+++ src/VisualStudio/TfsSourceControlFileSystemProviderPicker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Reflection;
+using EnvDTE80;
+
+namespace NuGet.VisualStudio
+{
+    [Export(typeof(ISourceControlFileSystemProvider))]
+    public class TfsSourceControlFileSystemProviderPicker : ISourceControlFileSystemProvider
+    {
+        private const string typeName = "NuGet.TeamFoundationServer.TfsSourceControlFileSystemProvider";
+
+        private ISourceControlFileSystemProvider _cachedTfsFileSystemProvider;
+
+        public ISourceControlFileSystem GetFileSystem(string path, SourceControlBindings binding)
+        {
+            ISourceControlFileSystemProvider tfsProvider = GetUnderlyingTfsProvider();
+            return tfsProvider != null ? tfsProvider.GetFileSystem(path, binding) : null;
+        }
+
+        private ISourceControlFileSystemProvider GetUnderlyingTfsProvider()
+        {
+            if (_cachedTfsFileSystemProvider == null)
+            {
+                var assemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+                string assemblyName = VsVersionHelper.IsVisualStudio2010 ? "NuGet.TeamFoundationServer10.dll" : "NuGet.TeamFoundationServer.dll";
+
+                try
+                {
+                    var assembly = Assembly.LoadFrom(Path.Combine(assemblyDirectory, assemblyName));
+                    if (assembly != null)
+                    {
+                        var type = assembly.GetType(typeName, throwOnError: false);
+                        if (type != null)
+                        {
+                            _cachedTfsFileSystemProvider = (ISourceControlFileSystemProvider)Activator.CreateInstance(type);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ExceptionHelper.WriteToActivityLog(ex);
+                    _cachedTfsFileSystemProvider = null;
+                }
+            }
+
+            return _cachedTfsFileSystemProvider;
+        }
+    }
+}
