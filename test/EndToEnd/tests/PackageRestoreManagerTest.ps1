32790bbc42a75a0d675058bb294e4285bc709e00
dotnetjunky <dotnetjunky@gmail.com>
2012-09-06 15:18:04 -0700

    Fix package restore not working when the 'packages' folder is missing. Work items: 1879

--- test/EndToEnd/tests/PackageRestoreManagerTest.ps1
+++ test/EndToEnd/tests/PackageRestoreManagerTest.ps1
@@ -20,6 +20,32 @@ function Test-EnablePackageRestoreOnlyModifyProjectsThatHaveInstalledPackages
     Assert-Null (Get-MsBuildPropertyValue $p2 "RestorePackages")
 }
 
+function Test-EnablePackageRestoreModifyProjectsThatHaveInstalledPackagesEventWhenThePackagesFolderIsMissing
+{
+    param($context)
+
+    # Arrange
+    $p1 = New-ClassLibrary
+    $p2 = New-ConsoleApplication
+
+    $p1 | Install-Package jQuery -Source $context.RepositoryPath
+    $p1.Save()
+    $p2.Save()
+
+    $packagesDir = Get-PackagesDir
+    Assert-True (Test-Path $packagesDir)
+    
+    Remove-Item $packagesDir -Recurse
+    Assert-False (Test-Path $packagesDir)
+
+    # Act
+    Enable-PackageRestore
+
+    # Assert
+    Assert-AreEqual "true" (Get-MsBuildPropertyValue $p1 "RestorePackages")
+    Assert-Null (Get-MsBuildPropertyValue $p2 "RestorePackages")
+}
+
 function Test-EnablePackageRestoreModifyProjectThatInstallNewPackages
 {
     param($context)
