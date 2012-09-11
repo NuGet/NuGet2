e8b4c1652e737729d386b3d442f78d6014d3b5fd
Pranav K <prkrishn@hotmail.com>
2012-09-11 13:53:04 -0700

    Ensure base path when building a package from a project is set to project root.
    Work Item: 2588

--- src/CommandLine/Commands/PackCommand.cs
+++ src/CommandLine/Commands/PackCommand.cs
@@ -105,6 +105,9 @@ namespace NuGet.Commands
 
             Console.WriteLine(NuGetResources.PackageCommandAttemptingToBuildPackage, Path.GetFileName(path));
 
+            // If the BasePath is not specified, use the directory of the input file (nuspec / proj) file
+            BasePath = String.IsNullOrEmpty(BasePath) ? Path.GetDirectoryName(Path.GetFullPath(path)) : BasePath;
+
             IPackage package = BuildPackage(path);
             if (package != null && !NoPackageAnalysis)
             {
@@ -112,7 +115,7 @@ namespace NuGet.Commands
             }
         }
 
-        private IPackage BuildPackage(string path, PackageBuilder builder, string outputPath = null)
+        private IPackage BuildPackage(PackageBuilder builder, string outputPath = null)
         {
             if (!String.IsNullOrEmpty(Version))
             {
@@ -121,8 +124,6 @@ namespace NuGet.Commands
 
             outputPath = outputPath ?? GetOutputPath(builder);
 
-            // If the BasePath is not specified, use the directory of the input file (nuspec / proj) file
-            BasePath = String.IsNullOrEmpty(BasePath) ? Path.GetDirectoryName(Path.GetFullPath(path)) : BasePath;
             ExcludeFiles(builder.Files);
             // Track if the package file was already present on disk
             bool isExistingPackage = File.Exists(outputPath);
@@ -276,7 +277,7 @@ namespace NuGet.Commands
                 }
             }
 
-            IPackage package = BuildPackage(path, packageBuilder);
+            IPackage package = BuildPackage(packageBuilder);
 
             if (Symbols)
             {
@@ -299,7 +300,7 @@ namespace NuGet.Commands
             }
 
             string outputPath = GetOutputPath(symbolsBuilder, symbols: true);
-            BuildPackage(path, symbolsBuilder, outputPath);
+            BuildPackage(symbolsBuilder, outputPath);
         }
 
         internal static void ExcludeFilesForLibPackage(ICollection<IPackageFile> files)
@@ -349,7 +350,7 @@ namespace NuGet.Commands
             PackageBuilder mainPackageBuilder = factory.CreateBuilder(BasePath);
 
             // Build the main package
-            IPackage package = BuildPackage(path, mainPackageBuilder);
+            IPackage package = BuildPackage(mainPackageBuilder);
 
             // If we're excluding symbols then do nothing else
             if (!Symbols)
@@ -366,7 +367,7 @@ namespace NuGet.Commands
 
             // Get the file name for the sources package and build it
             string outputPath = GetOutputPath(symbolsBuilder, symbols: true);
-            BuildPackage(path, symbolsBuilder, outputPath);
+            BuildPackage(symbolsBuilder, outputPath);
 
             // this is the real package, not the symbol package
             return package;
