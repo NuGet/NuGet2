8a42cc8f8d8029688242c1a8943d33f5e51eec0f
anurse <andrew@andrewnurse.net>
2012-11-02 12:06:37 -0700

    Minor cleanup, and added Debug helper which ONLY works on Debug builds.

---
+++ src/CommandLine/PackageSourceBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGet
+{
+    internal static class PackageSourceBuilder
+    {
+        internal static PackageSourceProvider CreateSourceProvider(ISettings settings)
+        {
+            var defaultPackageSource = new PackageSource(NuGetConstants.DefaultFeedUrl);
+
+            var officialPackageSource = new PackageSource(NuGetConstants.DefaultFeedUrl, NuGetResources.OfficialPackageSourceName);
+            var v1PackageSource = new PackageSource(NuGetConstants.V1FeedUrl, NuGetResources.OfficialPackageSourceName);
+            var legacyV2PackageSource = new PackageSource(NuGetConstants.V2LegacyFeedUrl, NuGetResources.OfficialPackageSourceName);
+
+            var packageSourceProvider = new PackageSourceProvider(
+                settings,
+                new[] { defaultPackageSource },
+                new Dictionary<PackageSource, PackageSource> { 
+                            { v1PackageSource, officialPackageSource },
+                            { legacyV2PackageSource, officialPackageSource }
+                        }
+            );
+            return packageSourceProvider;
+        }
+    }
+}
