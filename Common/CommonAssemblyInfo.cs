f04005a667fe49f30c782805cacb802b2439464f
dotnetjunky <dotnetjunky@gmail.com>
2012-11-06 16:04:35 -0800

    Bump the versions to 2.3

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 #if !FIXED_ASSEMBLY_VERSION
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyInformationalVersion("2.2.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyInformationalVersion("2.3.0")]
 #endif
 
 [assembly: NeutralResourcesLanguage("en-US")]
