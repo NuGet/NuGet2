c9e33af76a38e2b1c37e57eeed733333b6ead9ab
Olivier Dagenais <odagenais@jsitelecom.com>
2012-11-27 19:06:00 -0500

    Moved an integration test to the Test.Integration project.

--- test/CommandLine.Test/ProjectFactoryTest.cs
+++ test/CommandLine.Test/ProjectFactoryTest.cs
@@ -114,27 +114,5 @@ namespace NuGet.Test
             Assert.Equal("true", project.GetProperty("MyGlobalProperty").UnevaluatedValue, StringComparer.OrdinalIgnoreCase);
             Assert.Equal("true", project.GetProperty("TestProperty").UnevaluatedValue, StringComparer.OrdinalIgnoreCase);
         }
-
-        [Fact]
-        public void ProjectFactoryCanCompareContentsOfReadOnlyFile()
-        {
-            var us = Assembly.GetExecutingAssembly();
-            var sourcePath = us.Location;
-            var targetFile = new PhysicalPackageFile { SourcePath = sourcePath };
-            var fullPath = sourcePath + "readOnly";
-            File.Copy(sourcePath, fullPath);
-            File.SetAttributes(fullPath, FileAttributes.ReadOnly);
-            try
-            {
-                var actual = ProjectFactory.ContentEquals(targetFile, fullPath);
-
-                Assert.Equal(true, actual);
-            }
-            finally
-            {
-                File.SetAttributes(fullPath, FileAttributes.Normal);
-                File.Delete(fullPath);
-            }
-        }
     }
 }
