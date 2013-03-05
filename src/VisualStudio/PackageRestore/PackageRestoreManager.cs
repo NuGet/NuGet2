4c4bba977ccbb9e1094d2496f29c756477b76f75
dotnetjunky <dotnetjunky@gmail.com>
2013-03-04 16:01:33 -0800

    Remove reference to NuGet.VisualStudio12.

--- src/VisualStudio/PackageRestore/PackageRestoreManager.cs
+++ src/VisualStudio/PackageRestore/PackageRestoreManager.cs
@@ -293,9 +293,9 @@ namespace NuGet.VisualStudio
                 }
                 else
                 {
-                    NuGet.VisualStudio12.ProjectHelper.DoWorkInWriterLock(
-                        project.ToVsHierarchy(),
-                        buildProject => EnablePackageRestore(project, buildProject, saveProjectWhenDone: false));
+                    //NuGet.VisualStudio12.ProjectHelper.DoWorkInWriterLock(
+                    //    project.ToVsHierarchy(),
+                    //    buildProject => EnablePackageRestore(project, buildProject, saveProjectWhenDone: false));
                 }
 
                 // When inside the Write lock, calling Project.Save() will cause a deadlock.
