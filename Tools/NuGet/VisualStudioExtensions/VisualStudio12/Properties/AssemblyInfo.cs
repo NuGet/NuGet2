749d2e9624120d46bd2b9d4bf1a511b35ff31c27
dotnetjunky <dotnetjunky@gmail.com>
2013-03-04 16:41:32 -0800

    Add vs extensions projects.

---
+++ Tools/NuGet/VisualStudioExtensions/VisualStudio12/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VS12Extensions")]
+[assembly: AssemblyDescription("This assembly contains VS 2012-specific features.")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("99226151-c917-49b4-85fd-2d1d345e2703")]
