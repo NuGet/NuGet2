79751c1d514480a15e3ea51ad591a751e7ee01d9
dotnetjunky <dotnetjunky@gmail.com>
2013-04-09 15:23:28 -0700

    Disable the Update All button while an operation is in progress.

--- src/DialogServices/PackageManagerUI/UpdateAllUI.xaml.cs
+++ src/DialogServices/PackageManagerUI/UpdateAllUI.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using NuGet.Dialog.Providers;
 
 namespace NuGet.Dialog.PackageManagerUI
 {
@@ -13,16 +14,18 @@ namespace NuGet.Dialog.PackageManagerUI
             InitializeComponent();
         }
 
-        public event RoutedEventHandler UpdateInvoked
+        public event RoutedEventHandler UpdateInvoked = delegate {};
+
+        private void CommandBinding_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
         {
-            add
-            {
-                UpdateButton.Click += value;
-            }
-            remove
-            {
-                UpdateButton.Click -= value;
-            }
+            e.CanExecute = !OperationCoordinator.IsBusy;
+            e.Handled = true;
+        }
+
+        private void CommandBinding_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            UpdateInvoked(this, e);
+            e.Handled = true;
         }
     }
 }
