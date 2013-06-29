d067d33493574ea2bc156ef680f2a97c4d809d7d
dotnetjunky <dotnetjunky@gmail.com>
2013-06-29 05:43:51 -0700

    update reference assemblies for VS 2012

--- Tools/NuGet/VisualStudioExtensions/ExtensionsExplorer11/Properties/AssemblyInfo.cs
+++ Tools/NuGet/VisualStudioExtensions/ExtensionsExplorer11/Properties/AssemblyInfo.cs
@@ -1,26 +1,30 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VisualStudio.Types")]
+[assembly: AssemblyTitle("Microsoft.VisualStudio.ExtensionsExplorer.dll")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VisualStudio.Types")]
+[assembly: AssemblyProduct("Microsoft.VisualStudio.ExtensionsExplorer.dll")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("200ffe61-68ea-49a2-bb09-dd4153688ef4")]
+[assembly: Guid("bdaaa5c6-5b37-4bad-8b7e-4514c1174cf5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +36,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("11.0.0.0")]
+[assembly: AssemblyFileVersion("11.0.20304.0")]
