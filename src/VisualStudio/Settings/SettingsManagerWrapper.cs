83cadf2cc4aa0443db4374da47ed78d0b9f1f2be
dotnetjunky <dotnetjunky@gmail.com>
2013-08-14 15:58:12 -0700

    Remove referfence to Microsoft.VisualStudio.Settings. Work items: 3548

--- src/VisualStudio/Settings/SettingsManagerWrapper.cs
+++ src/VisualStudio/Settings/SettingsManagerWrapper.cs
@@ -1,26 +1,44 @@
 ﻿using System;
-using Microsoft.VisualStudio.Settings;
+using System.Diagnostics;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Shell.Settings;
 
 namespace NuGet.VisualStudio
 {
     internal class SettingsManagerWrapper : ISettingsManager
     {
-        private readonly ShellSettingsManager _settingsManager;
+        private readonly IVsSettingsManager _settingsManager;
 
         public SettingsManagerWrapper(IServiceProvider serviceProvider)
         {
-            _settingsManager = new ShellSettingsManager(serviceProvider);
+            _settingsManager = (IVsSettingsManager)serviceProvider.GetService(typeof(SVsSettingsManager));
+            Debug.Assert(_settingsManager != null);
         }
 
         public ISettingsStore GetReadOnlySettingsStore()
         {
-            return new SettingsStoreWrapper(_settingsManager.GetReadOnlySettingsStore(SettingsScope.UserSettings));
+            IVsSettingsStore settingsStore;
+            int hr = _settingsManager.GetReadOnlySettingsStore((uint)__VsSettingsScope.SettingsScope_UserSettings, out settingsStore);
+            if (ErrorHandler.Succeeded(hr) && settingsStore != null)
+            {
+                return new SettingsStoreWrapper(settingsStore);
+            }
+
+            return null;
         }
 
         public IWritableSettingsStore GetWritableSettingsStore()
         {
-            return new WritableSettingsStoreWrapper(_settingsManager.GetWritableSettingsStore(SettingsScope.UserSettings));
+            IVsWritableSettingsStore settingsStore;
+
+            int hr = _settingsManager.GetWritableSettingsStore((uint)__VsSettingsScope.SettingsScope_UserSettings, out settingsStore);
+            if (ErrorHandler.Succeeded(hr) && settingsStore != null)
+            {
+                return new WritableSettingsStoreWrapper(settingsStore);
+            }
+
+            return null;
         }
     }
 }
