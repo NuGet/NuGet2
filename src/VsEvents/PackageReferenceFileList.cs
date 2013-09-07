495b25822b76e2703c53c34ca26e8b10f248fd3b
feiling <feiling@microsoft.com>
2013-09-06 17:04:37 -0700

    Fix the bug that VS Package Restore skips solution folders.
    The fix is simple: just traverse subprojects if the current project is a solution folder.
    Other changes in this change set:
    - Created new helper class SettingsHelper so that functional tests can use it to change NuGet user settings.
    - Updated VS package restore functional tests so that the test solutions now contain solution folders.

---
+++ src/VsEvents/PackageReferenceFileList.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using EnvDTE;
+using NuGet.VisualStudio;
+
+namespace NuGet.VsEvents
+{
+    /// <summary>
+    /// This class contains the list of all package reference files used by a solution and all projects
+    /// contained in the solution.
+    /// </summary>
+    internal class PackageReferenceFileList
+    {
+        // the full path of the package reference file in the nuget solution folder ($solutionDir\.nuget).
+        private readonly string _solutionPackageReferenceFile;
+
+        // the collection of package reference files used by projects in the solution.
+        private readonly List<ProjectPackageReferenceFile> _projectPackageReferenceFiles;
+
+        public PackageReferenceFileList(Solution solution)
+        {
+            _projectPackageReferenceFiles = new List<ProjectPackageReferenceFile>();
+
+            var packageReferenceFileName = Path.Combine(
+                VsUtility.GetNuGetSolutionFolder(solution),
+                VsUtility.PackageReferenceFile);
+            if (File.Exists(packageReferenceFileName))
+            {
+                _solutionPackageReferenceFile = packageReferenceFileName;
+            }
+
+            foreach (Project project in solution.Projects)
+            {
+                GetPackageReferenceFiles(project);
+            }
+        }
+
+        public string SolutionPackageReferenceFile
+        {
+            get { return _solutionPackageReferenceFile; }
+        }
+
+        public ICollection<ProjectPackageReferenceFile> ProjectPackageReferenceFiles
+        {
+            get { return _projectPackageReferenceFiles; }
+        }
+
+        /// <summary>
+        /// Indicates whether the solution itself and projects contained in the solution use any 
+        /// package reference files. True if no package reference files are used.
+        /// </summary>
+        public bool IsEmpty
+        {
+            get
+            {
+                return _solutionPackageReferenceFile == null && _projectPackageReferenceFiles.Count == 0;
+            }
+        }
+
+        /// <summary>
+        /// Gets package reference file(s) used by the project, or subprojects if the project is a solution folder,
+        /// and adds the files into list _projectPackageReferenceFiles.
+        /// </summary>
+        /// <param name="project">the project to inspect.</param>
+        private void GetPackageReferenceFiles(Project project)
+        {
+            if (project == null)
+            {
+                return;
+            }
+
+            if (VsConstants.VsProjectItemKindSolutionFolder.Equals(project.Kind, StringComparison.OrdinalIgnoreCase))
+            {
+                foreach (ProjectItem item in project.ProjectItems)
+                {
+                    var nestedProject = item.SubProject;
+                    GetPackageReferenceFiles(nestedProject);
+                }
+            }
+            else
+            {
+                var packageReferenceFileName = VsUtility.GetPackageReferenceFileFullPath(project);
+                if (File.Exists(packageReferenceFileName))
+                {
+                    _projectPackageReferenceFiles.Add(new ProjectPackageReferenceFile(project, packageReferenceFileName));
+                }
+            }
+        }
+    }
+}
