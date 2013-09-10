b0dbfe7e8aeecec1c7f41ca8e48b147a97c97172
feiling <feiling@microsoft.com>
2013-09-10 16:19:10 -0700

    Instead of creating one target per imported targets file, create one target for all imported targets files.

--- Common/MsBuildProjectUtility.cs
+++ Common/MsBuildProjectUtility.cs
@@ -1,61 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Runtime.Remoting.Metadata.W3cXsd2001;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Build.Construction;
 using MsBuildProject = Microsoft.Build.Evaluation.Project;
 
 namespace NuGet
 {
     public static class MSBuildProjectUtility
     {
+        private const string targetName = "EnsureNuGetPackageBuildImports";
+
         public static void AddEnsureImportedTarget(MsBuildProject buildProject, string targetsPath)
         {
-            // first remove the target if it already exits.
-            RemoveEnsureImportedTarget(buildProject, targetsPath);
+            // get the target
+            var targetElement = buildProject.Xml.Targets.FirstOrDefault(
+                target => target.Name.Equals(targetName, StringComparison.OrdinalIgnoreCase));
 
-            var targetName = GetNameOfEnsureImportedTarget(targetsPath);
-            var target = buildProject.Xml.AddTarget(targetName);
-            target.Condition = "!Exists('" + targetsPath + "')";
+            // if the target does not exist, create the target
+            if (targetElement == null)
+            {
+                targetElement = buildProject.Xml.AddTarget(targetName);
 
-            // PrepareForBuild is used here because BeforeBuild does not work for VC++ projects.
-            target.BeforeTargets = "PrepareForBuild";
+                // PrepareForBuild is used here because BeforeBuild does not work for VC++ projects.
+                targetElement.BeforeTargets = "PrepareForBuild";
 
-            var errorTask = target.AddTask("Error");
+                var propertyGroup = targetElement.AddPropertyGroup();
+                propertyGroup.AddProperty("ErrorText", CommonResources.EnsureImportedMessage);
+            }
+
+            var errorTask = targetElement.AddTask("Error");
+            errorTask.Condition = "!Exists('" + targetsPath + "')";
             var errorText = string.Format(
                 CultureInfo.InvariantCulture,
-                CommonResources.EnsureImportedMessage,
+                @"$([System.String]::Format('$(ErrorText)', '{0}'))",
                 targetsPath);
             errorTask.SetParameter("Text", errorText);
         }
 
         public static void RemoveEnsureImportedTarget(MsBuildProject buildProject, string targetsPath)
         {
-            if (buildProject.Xml.Targets != null)
+            var targetElement = buildProject.Xml.Targets.FirstOrDefault(
+                target => string.Equals(target.Name, targetName, StringComparison.OrdinalIgnoreCase));
+            if (targetElement == null)
             {
-                var targetName = GetNameOfEnsureImportedTarget(targetsPath);
-                var targetElement = buildProject.Xml.Targets.FirstOrDefault(
-                    target => target.Name.Equals(targetName, StringComparison.OrdinalIgnoreCase));
-                if (targetElement != null)
-                {
-                    targetElement.Parent.RemoveChild(targetElement);
-                }
+                return;
             }
-        }
 
-        /// <summary>
-        /// Returns the name of the target that is used to ensure targetsPath is imported.
-        /// </summary>
-        /// <param name="targetsPath">The targetsPath.</param>
-        /// <returns>the name of the target.</returns>
-        private static string GetNameOfEnsureImportedTarget(string targetsPath)
-        {
-            var pathBytes = Encoding.UTF8.GetBytes(targetsPath);
-            var hashProvider = new CryptoHashProvider("SHA256");
-            var hash = new SoapHexBinary(hashProvider.CalculateHash(pathBytes)).ToString();
-            return "EnsureImported" + hash;
+            string errorCondition = "!Exists('" + targetsPath + "')";
+            var taskElement = targetElement.Tasks.FirstOrDefault(
+                task => string.Equals(task.Condition, errorCondition, StringComparison.OrdinalIgnoreCase));
+            if (taskElement == null)
+            {
+                return;
+            }
+
+            taskElement.Parent.RemoveChild(taskElement);
+            if (targetElement.Tasks.Count == 0)
+            {
+                targetElement.Parent.RemoveChild(targetElement);
+            }
         }
     }
 }
