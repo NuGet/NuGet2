39bc96a7c30b7c545a5c60cb04ea943b63eb97ad
juste <juste@microsoft.com>
2014-01-15 15:27:38 -0800

    UI changes to add tooltips and display dates in local time

---
+++ src/DialogServices/PackageManagerUI/Converters/DateTimeOffsetToLocalConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NuGet.Dialog.PackageManagerUI
+{
+    /// <summary>
+    /// Converts a DateTimeOffset to the local time zone.
+    /// </summary>
+    public class DateTimeOffsetToLocalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTimeOffset? offset = value as DateTimeOffset?;
+
+            if (offset.HasValue)
+            {
+                return offset.Value.ToLocalTime();
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
