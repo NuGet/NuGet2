d2cea6818c5bdecef3a9426d198c9a9103b3a083
feiling <feiling@microsoft.com>
2014-02-13 16:11:18 -0800

    Fix the problem that the web request is displayed twice when -Verbosity is set to detailed.
    The cause of the duplicate output is the change to fix issue 3801 (which
    was caused by incorrect use of the weak  event handler pattern). With
    that fix, the SendingRequest handler will be called twice. The fix is to
    use the weak event handler pattern here too.

--- src/CommandLine/Common/CommandLineRepositoryFactory.cs
+++ src/CommandLine/Common/CommandLineRepositoryFactory.cs
@@ -1,7 +1,9 @@
 ﻿
+using System.Windows;
+
 namespace NuGet.Common
 {
-    public class CommandLineRepositoryFactory : PackageRepositoryFactory
+    public class CommandLineRepositoryFactory : PackageRepositoryFactory, IWeakEventListener
     {
         public static readonly string UserAgent = "NuGet Command Line";
 
@@ -16,23 +18,33 @@ namespace NuGet.Common
         {
             var repository = base.CreateRepository(packageSource);
             var httpClientEvents = repository as IHttpClientEvents;
-
             if (httpClientEvents != null)
             {
-                httpClientEvents.SendingRequest += (sender, args) =>
-                {
-                    if (_console.Verbosity == Verbosity.Detailed)
-                    {
-                        _console.WriteLine(
-                            System.ConsoleColor.Green,
-                            "{0} {1}", args.Request.Method, args.Request.RequestUri);
-                    }
-                    string userAgent = HttpUtility.CreateUserAgentString(CommandLineConstants.UserAgent);
-                    HttpUtility.SetUserAgent(args.Request, userAgent);
-                };
+                SendingRequestEventManager.AddListener(httpClientEvents, this);
             }
 
             return repository;
+        }
+
+        public bool ReceiveWeakEvent(System.Type managerType, object sender, System.EventArgs e)
+        {
+            if (managerType == typeof(SendingRequestEventManager))
+            {
+                var args = (WebRequestEventArgs)e;
+                if (_console.Verbosity == Verbosity.Detailed)
+                {
+                    _console.WriteLine(
+                        System.ConsoleColor.Green,
+                        "{0} {1}", args.Request.Method, args.Request.RequestUri);
+                }
+                string userAgent = HttpUtility.CreateUserAgentString(CommandLineConstants.UserAgent);
+                HttpUtility.SetUserAgent(args.Request, userAgent);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
