b2fea0a4515974f369c1e73ba19b13fb500e041b
juste <juste@microsoft.com>
2014-02-21 14:52:14 -0800

    GetFullPath fallback

--- test/VisualStudio.Test/VsUtilityTest.cs
+++ test/VisualStudio.Test/VsUtilityTest.cs
@@ -1,6 +1,7 @@
 ﻿using EnvDTE;
 using Moq;
 using Xunit;
+using Xunit.Extensions;
 
 namespace NuGet.VisualStudio.Test
 {
@@ -22,6 +23,43 @@ namespace NuGet.VisualStudio.Test
             project.SetupGet(p => p.DTE).Returns(dte.Object);
             project.SetupGet(p => p.UniqueName).Returns(@"b\c.csproj");
             project.SetupGet(p => p.Kind).Returns(VsConstants.UnloadedProjectTypeGuid);
+
+            // Act
+            var projectDirectory = VsUtility.GetFullPath(project.Object);
+
+            // Assert
+            Assert.Equal(@"c:\a\b", projectDirectory);
+        }
+
+        [Theory]
+        [InlineData("FullPath", @"c:\a\b", @"c:\a\b")]
+        [InlineData("ProjectDirectory", @"c:\a\b", @"c:\a\b")]
+        [InlineData("FullName", @"c:\a\b\c.csproj", @"c:\a\b")]
+        public void GetFullPathFallback(string propName, string propValue, string expected)
+        {
+            // Arrange
+            var solution = new Mock<Solution>();
+            solution.SetupGet(s => s.FullName).Returns(@"c:\a\s.sln");
+
+            var dte = new Mock<DTE>();
+            dte.SetupGet(d => d.Solution).Returns(solution.Object);
+
+            var property = new Mock<EnvDTE.Property>();
+            property.SetupGet(p => p.Value).Returns(propValue);
+
+            var properties = new Mock<EnvDTE.Properties>();
+            properties.Setup(p => p.Item(propName)).Returns(property.Object);
+
+            var project = new Mock<Project>();
+            project.SetupGet(p => p.DTE).Returns(dte.Object);
+            project.SetupGet(p => p.UniqueName).Returns(@"b\c.csproj");
+            project.SetupGet(p => p.Kind).Returns(VsConstants.CsharpProjectTypeGuid);
+            project.SetupGet(p => p.Properties).Returns(properties.Object);
+
+            if (propName == "FullName")
+            {
+                project.SetupGet(p => p.FullName).Returns(propValue);
+            }
 
             // Act
             var projectDirectory = VsUtility.GetFullPath(project.Object);
