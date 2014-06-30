b984a258dbf8ceaca9dee0e154246f72d91f2761
Dan Liu <danliu@microsoft.com>
2014-06-30 16:54:35 -0700

    fix dev14 tests

--- test/EndToEnd/vs.ps1
+++ test/EndToEnd/vs.ps1
@@ -253,7 +253,7 @@ function New-JavaScriptApplication
 
     try 
     {
-        if ($dte.Version -eq '12.0')
+        if ($dte.Version -eq '12.0' -or $dte.Version -eq '14.0')
         {
             $SolutionFolder | New-Project WinJSBlue $ProjectName
         }
@@ -314,7 +314,7 @@ function New-NativeWinStoreApplication
 
     try
     {
-        if ($dte.Version -eq '12.0')
+        if ($dte.Version -eq '12.0' -or $dte.Version -eq '14.0')
         {
             $SolutionFolder | New-Project CppWinStoreApplicationBlue $ProjectName
         }
