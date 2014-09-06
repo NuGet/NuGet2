f865d858bf2640ccaa7f790b757a13df474311b1
Jeff Handley <jeff@jeffhandley.com>
2014-09-06 01:56:44 -0700

    Fixing a typo for propertyConstraint serialization. Adding a couple more tests.

--- src/Core/Authoring/ManifestDependency.cs
+++ src/Core/Authoring/ManifestDependency.cs
@@ -17,7 +17,7 @@ namespace NuGet
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly", Justification = "This is required by XML serializer")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1002:DoNotExposeGenericLists", Justification = "This is required by XML serializer.")]
-        [XmlElement("propertyConstraints")]
+        [XmlElement("propertyConstraint")]
         public List<ManifestProperty> PropertyConstraints { get; set; }
     }
 }
