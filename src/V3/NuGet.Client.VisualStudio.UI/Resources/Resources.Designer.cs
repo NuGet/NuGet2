be25630765e835b4b6037af630ac625bd1b07384
feiling <feiling@microsoft.com>
2014-09-10 10:21:51 -0700

    Reorg NuGet Visual Studio UI project structure

--- src/V3/NuGet.Client.VisualStudio.UI/Resources/Resources.Designer.cs
+++ src/V3/NuGet.Client.VisualStudio.UI/Resources/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace NuGet.Client.VisualStudio.UI {
+namespace NuGet.Client.VisualStudio.UI.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace NuGet.Client.VisualStudio.UI {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NuGet.Client.VisualStudio.UI.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NuGet.Client.VisualStudio.UI.Resources.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
