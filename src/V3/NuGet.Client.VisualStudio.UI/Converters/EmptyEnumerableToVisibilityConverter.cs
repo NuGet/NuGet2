6ccab264daa4f02151e39020b5ae3f9538618c63
feiling <feiling@microsoft.com>
2014-10-21 13:18:12 -0700

    Show "No dependencies" if the dependency list is empty for a target framework.

---
+++ src/V3/NuGet.Client.VisualStudio.UI/Converters/EmptyEnumerableToVisibilityConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NuGet.Client.VisualStudio.UI
+{
+    /// <summary>
+    /// If the value is an empty or null IEnumerable, returns Visibility.Visible.
+    /// Otherwise, returns Visibility.Collapsed.
+    /// </summary>
+    public class EmptyEnumerableToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (targetType == typeof(Visibility))
+            {
+                var list = value as IEnumerable;
+                if (IsNullOrEmpty(list))
+                {
+                    return Visibility.Visible;
+                }
+
+                return Visibility.Collapsed;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsNullOrEmpty(IEnumerable list)
+        {
+            if (list == null)
+            {
+                return true;
+            }
+
+            var enumerator = list.GetEnumerator();
+            return enumerator.MoveNext() == false;
+        }
+    }
+}
