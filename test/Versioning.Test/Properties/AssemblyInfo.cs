26b151685df28e319339d307cd6d57c9a315b247
feiling <feiling@microsoft.com>
2014-12-10 14:42:12 -0800

    Delete project Versioning. Replace it with NuGet.Versioning package.

--- test/Versioning.Test/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("NuGet.Versioning.Test")]
-[assembly: AssemblyDescription("Unit tests for NUGet.Versioning.dll")]
