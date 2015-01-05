aa5272116005edebb8fb47691da6e263926c825b
bhuvak <bhuvak@live.com>
2015-01-05 10:43:19 -0800

    Added providers for VisualStudio metadata and search

---
+++ src/NuGet.Client.V2.VisualStudio/V2PowerShellAutocompleteResourceProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+using NuGet.Client.VisualStudio.Models;
+
+namespace NuGet.Client.V2.VisualStudio
+{
+    [Export(typeof(ResourceProvider))]
+    [ResourceProviderMetadata("V2PowerShellAutocompleteResourceProvider", typeof(IPowerShellAutoComplete))]
+    public class V2PowerShellAutocompleteResourceProvider : V2ResourceProvider
+    {
+        public override async System.Threading.Tasks.Task<Resource> Create(PackageSource source)
+        {
+            var resource = await base.Create(source);
+            if (resource != null)
+            {
+                var vsV2SearchResource = new V2PowerShellAutocompleteResource((V2Resource)resource);
+                return vsV2SearchResource;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ src/NuGet.Client.V2.VisualStudio/V2VisualStudioUIMetatdataResourcerProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+using NuGet.Client.VisualStudio.Models;
+
+namespace NuGet.Client.V2.VisualStudio
+{
+    [Export(typeof(ResourceProvider))]
+    [ResourceProviderMetadata("V2VisualStudioUIMetatdataResourceProvider", typeof(IVisualStudioUIMetadata))]
+    public class V2VisualStudioUIMetadataResourceProvider : V2ResourceProvider
+    {
+        public override async System.Threading.Tasks.Task<Resource> Create(PackageSource source)
+        {
+            var resource = await base.Create(source);
+            if (resource != null)
+            {
+                var vsV2MetatdataResource = new V2VisualStudioUIMetadataResource((V2Resource)resource);
+                return vsV2MetatdataResource;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
