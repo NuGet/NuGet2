a99aa79889e95b587fee6e8165acd7d286391094
Justin Emgarten <justin@emgarten.com>
2015-07-23 11:26:48 -0700

    Updating to 2.8.8

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.InteropServices;
 // Build\Build.proj.
 // When built locally, the NuGet release version is the values specified in this file.
 #if !FIXED_ASSEMBLY_VERSION
-[assembly: AssemblyVersion("2.8.7.0")]
-[assembly: AssemblyInformationalVersion("2.8.7")]
+[assembly: AssemblyVersion("2.8.8.0")]
+[assembly: AssemblyInformationalVersion("2.8.8")]
 #endif
 
 [assembly: NeutralResourcesLanguage("en-US")]
