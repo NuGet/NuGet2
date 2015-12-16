92ce0fa95bf09984a41609f48c2d3a0dd54b5a8f
Alex Panov <alpaix@outlook.com>
2015-12-16 12:14:32 -0800

    Updated version to 2.11

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.InteropServices;
 // Build\Build.proj.
 // When built locally, the NuGet release version is the values specified in this file.
 #if !FIXED_ASSEMBLY_VERSION
-[assembly: AssemblyVersion("2.10.1.0")]
-[assembly: AssemblyInformationalVersion("2.10.1")]
+[assembly: AssemblyVersion("2.11.0.0")]
+[assembly: AssemblyInformationalVersion("2.11.0")]
 #endif
 
 [assembly: NeutralResourcesLanguage("en-US")]
