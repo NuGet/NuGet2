06b6e655a8cc4852417fa6ebdcf8168cbffd4320
Rohit Agrawal <ragrawal@microsoft.com>
2016-07-01 10:55:01 -0700

    bump to version 2.13

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.InteropServices;
 // Build\Build.proj.
 // When built locally, the NuGet release version is the values specified in this file.
 #if !FIXED_ASSEMBLY_VERSION
-[assembly: AssemblyVersion("2.12.0.0")]
-[assembly: AssemblyInformationalVersion("2.12.0")]
+[assembly: AssemblyVersion("2.13.0.0")]
+[assembly: AssemblyInformationalVersion("2.13.0")]
 #endif
 
 [assembly: NeutralResourcesLanguage("en-US")]
